5be127e033fec0974360ee69667df7613ae9c92b
MFournierQC <50327179+MFournierQC@users.noreply.github.com>
2019-05-29 14:26:12 -0400

    Work on metadata utests and some cleaning up.

--- Database/README.md
+++ Database/README.md
@@ -1,2 +1,3 @@
-Décrire ici l'utilisation du code présent dans ce répertoire.
+# Overview
+The files contained in this directory are for the extraction of data from CSV files and XML files. The code is then meant to store all of that data into a SQLite3 database.
 
--- Database/management_xml/metadata.py
+++ Database/management_xml/metadata.py
@@ -2,84 +2,148 @@ import xml.etree.ElementTree as ET
 import ImageAnalysis.source.cziUtil as czi
 from Database.management_xml.filter import Filter
 from Database.management_xml.channel import Channel
-import os
-import fnmatch
-
-
+'''
 def findAllCziFiles(path):
-    print('Walking the directory...')
     allCZIs = []
     for root, directories, files in os.walk(os.path.normpath(path)):
         for file in files:
             if fnmatch.fnmatch(file, '*.czi'):
                 allCZIs.append([file, os.path.join(root, file)])
-    print('...Done! ' + str(len(allCZIs)) + ' files found!')
     return allCZIs
+'''
 
 
 class Metadata:
     def __init__(self, path):
         self.path = path
-        self.root = self.createRoot()
+        self.root = None
 
-        self.filters = self.setFilters()
-        self.channels = self.setChannels()
+        # Filters and channels are lists of objects.
+        self.filters = None
+        self.channels = None
 
-        self.microscope = self.setMicroscope()
-        self.objective = self.setObjective()
-        self.xScale, self.yScale = self.setXYScale()
-        self.xSize, self.ySize = self.setXYSize()
-        self.xScaled, self.yScaled = self.setXYScaled()
+        self.microscope = None
+        self.objective = None
+        self.xScale = 0
+        self.yScale = 0
+        self.xSize = 0
+        self.ySize = 0
+        self.xScaled = 0
+        self.yScaled = 0
 
     def showData(self):  # TODO This might need to be modified. Currently only for testing purpose.
         print(self.filters)
         print(self.channels)
 
-    def createRoot(self):
-        cziImageObject = czi.readCziImage(self.path)
-        stringXML = czi.extractMetadataFromCziFileObject(cziImageObject)
+    def cziFileToCziImageObject(self):
+        try:
+            return czi.readCziImage(self.path)
+        except FileNotFoundError as error:
+            raise error
+        except ValueError as error:
+            raise error
+
+    def extractXmlAsStringFromCziImageObject(self, cziImageObject):
+        try:
+            return czi.extractMetadataFromCziFileObject(cziImageObject)
+        except AttributeError as error:
+            raise error
+
+    def createElementTreeRoot(self):
+        cziImageObject = self.cziFileToCziImageObject()
+        stringXML = self.extractXmlAsStringFromCziImageObject(cziImageObject)
         return ET.fromstring(stringXML)
 
+    def setAttributesFromXml(self):
+        self.root = self.createElementTreeRoot()
+
+        self.filters = self.setFiltersData()
+        self.channels = self.setChannels()
+
+        self.microscope = self.setMicroscope()
+        self.objective = self.setObjective()
+        self.xScale = self.setXScale()
+        self.yScale = self.setYScale()
+        self.xSize = self.setXSize()
+        self.ySize = self.setYSize()
+        self.xScaled = self.setXScaled()
+        self.yScaled = self.setYScaled()
+
     def setMicroscope(self):
-        for microscope in self.root.find('./Metadata/Information/Instrument/Microscopes'):
-            return microscope.attrib['Name']
+        try:
+            return self.root.find('./Metadata/Information/Instrument/Microscopes/Microscope').attrib['Name']
+        except KeyError as error:
+            raise error
+        except AttributeError as error:
+            raise error
 
     def setObjective(self):
-        for objective in self.root.find('./Metadata/Information/Instrument/Objectives'):
-            return objective.attrib['Name']
+        try:
+            return self.root.find('./Metadata/Information/Instrument/Objectives/Objective').attrib['Name']
+        except KeyError as error:
+            raise error
+        except AttributeError as error:
+            raise error
 
-    def setXYScale(self):
-        lstScale = []
-        for distance in self.root.find('./Metadata/Scaling/Items'):
-            for entry in distance:
-                if entry.tag == 'Value':
-                    lstScale.append(entry.text)
-        return lstScale[0], lstScale[1]
+    def setXScale(self):
+        try:
+            return self.root.find('./Metadata/Scaling/Items/Distance[@Id="X"]/Value').text
+        except AttributeError as error:
+            raise error
 
-    def setXYSize(self):
-        tags = ['SizeX', 'SizeY']
-        lstSize = []
-        for imageData in self.root.find('./Metadata/Information/Image'):
-            if imageData.tag in tags:
-                lstSize.append(imageData.text)
-        return lstSize[0], lstSize[1]
+    def setYScale(self):
+        try:
+            return self.root.find('./Metadata/Scaling/Items/Distance[@Id="Y"]/Value').text
+        except AttributeError as error:
+            raise error
 
-    def setXYScaled(self):
-        xScaled = float(self.xSize) * float(self.xScale)
-        yScaled = float(self.ySize) * float(self.yScale)
-        return xScaled, yScaled
+    def setXSize(self):
+        try:
+            return self.root.find('./Metadata/Information/Image/SizeX').text
+        except AttributeError as error:
+            raise error
 
-    def setFilters(self):
-        lstFilters = []
-        for filter in self.root.find('./Metadata/Information/Instrument/Filters'):
-            data = [filter.attrib['Id']]
-            for cut in filter.find('./TransmittanceRange'):
-                data.append(cut.text)
-            lstFilters.append(Filter(data[0], data[1], data[2]))
+    def setYSize(self):
+        try:
+            return self.root.find('./Metadata/Information/Image/SizeY').text
+        except AttributeError as error:
+            raise error
 
-        for filter in lstFilters:
-            filter.setFilterData(self.root)
-        return lstFilters
+    def setXScaled(self):
+        try:
+            return float(self.xSize) * float(self.xScale)
+        except ValueError as error:
+            raise error
+
+    def setYScaled(self):
+        try:
+            return float(self.ySize) * float(self.yScale)
+        except ValueError as error:
+            raise error
+
+    def findFiltersEntriesInXml(self):
+        try:
+            filters = []
+            for filter in self.root.find('./Metadata/Information/Instrument/Filters'):
+                filterId = filter.attrib['Id']
+                cutIn = filter.find('./TransmittanceRange/CutIn').text
+                cutOut = filter.find('./TransmittanceRange/CutOut').text
+
+                filters.append(Filter(filterId, cutIn, cutOut))
+            return filters
+        except AttributeError as error:
+            raise error
+        except KeyError as error:
+            raise error
+
+    def setFiltersData(self):
+        try:
+            filters = self.findFiltersEntriesInXml()
+            for filter in filters:
+                filter.setFilterData(self.root)
+            return filters
+        except Exception as error:
+            raise error
 
     def setChannels(self):
         lstChannels = []
@@ -89,13 +153,4 @@ class Metadata:
             lstChannels.append(newChannel)
 
         return lstChannels
-
-
-# For testing purpose. To be removed.
-if __name__ == '__main__':
-    directory = os.path.dirname(os.path.dirname(__file__))
-    filepath = os.path.join(directory, 'temporary_files', 'testCziFile.czi')
-
-    mdata = Metadata(filepath)
-    mdata.showData()
 
