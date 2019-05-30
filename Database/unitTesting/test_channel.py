8468e9f91add358ba41db7d2a695605eb9730653
MFournierQC <50327179+MFournierQC@users.noreply.github.com>
2019-05-30 14:51:05 -0400

    Miscellaneous tests and beginning to work on channel testing.

---
+++ Database/unitTesting/test_channel.py
@@ -0,0 +1,14 @@
+from filter import Filter as fltr
+from metadata import Metadata as mtdt
+import xml.etree.ElementTree as ET
+import unittest
+import os
+
+
+class TestChannel(unittest.TestCase):
+    def setUp(self) -> None:
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
