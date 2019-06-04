d0d00a8d5c80cf5bfb25f218a80a22d7808254b5
MFournierQC <50327179+MFournierQC@users.noreply.github.com>
2019-06-04 08:36:02 -0400

    Work on a small script to use as demo.

--- Database/CziMetadataManagement/reader.py
+++ Database/CziMetadataManagement/reader.py
@@ -14,15 +14,26 @@ def getMetadataFromFiles(cziFiles):
     for cziFile in cziFiles:
         newMetadata = mtdt(cziFile[1], cziFile[0])
         newMetadata.setAttributesFromXml()
-
+        getMetadataFromFilesName(newMetadata)
         metadataObjects.append(newMetadata)
     return metadataObjects
 
+
 def getMetadataFromFilesName(metadata):
-    pass
+    name = metadata.getName().split('_')
+    vectors = []
+    mouseId = ''
+    for cell in name:
+        if re.match(r'^\w{3}\d{3}', cell):
+            vectors.append(re.match(r'^\w{3}\d{3}', cell).group())
+        if re.match(r'^\w\d{3}', cell):
+            mouseId = re.match(r'^\w\d{3}', cell).group()[1:]
+    metadata.setMouseId(mouseId)
+    metadata.setVectors(vectors)
 
 
 if __name__ == '__main__':
+    '''
     # We find all of the czi files in a given folder.
     allCziFiles = czi.findAllCziFiles('P:\\injection AAV\\résultats bruts\\2019-01-23')
 
@@ -50,3 +61,16 @@ if __name__ == '__main__':
         # Small function to print export all of the channel's testData.
         for c in m.getChannels():
             print('---Channel in Metdata : ', c.exportDataAsDict())
+    '''
+    path = 'P:\\injection AAV\\résultats bruts\\2019-01-23'
+    metadatas = getMetadataFromCzis(path)
+
+    for metadata in metadatas:
+        print('THIS IS METADATA RELATED TO THE FILE : ', metadata.getName())
+        for key, value in metadata.exportDataAsDict().items():
+            print(key, " : ", value)
+        print('ITS CHANNELS ARE : ')
+        for channel in metadata.getChannels():
+            print(channel.channelName)
+            for key, value in channel.exportDataAsDict().items():
+                print(key, " : ", value)
