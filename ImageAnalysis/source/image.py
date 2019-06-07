14a46abd8cafbca336729550f6210fc929556fc6
Gabriel Genest <gabriel.genest.2@ulaval.ca>
2019-06-07 16:00:22 -0400

    Worked on image.py and cziUtil. Made a new method to read the data form czi files in cziUtil based on the one from czifile, but modified to get all the tiles in the mosaic, as well as the mosaic. Also, the problem of big czi files is due to the access to the server. When the file is on my computer, it is very fast.

--- ImageAnalysis/source/image.py
+++ ImageAnalysis/source/image.py
@@ -1,13 +1,15 @@
 from channel import *
 import cziUtil
 import re
+import os
+import time
 
 
 class Image:
 
     def __init__(self, path: str):
         self.__path = path
-        self.image = self.__findRightReader(path)
+        self.__image = self.__findRightReader(path)
 
     @staticmethod
     def __findRightReader(path: str):
@@ -21,25 +23,40 @@ class Image:
             image = _ImageFromOtherFile(path)
         return image
 
+    def getChannel(self, index: int):
+        return self.__image.getChannel(index)
+
 
 class _ImageFromCZIFile:
 
     def __init__(self, path: str):
+        self.__fileOpened = True
+        if os.path.getsize(path) > 1024 * 1024 * 2000:
+            self.__fileOpened = False
+            raise Exception("File too big to load.")
         self.__cziObj = cziUtil.readCziImage(path)
-        self.__imagesDirectory = self.__cziObj.filtered_subblock_directory
-        self.__subblocks = self.__cziObj.subblocks()
-        # if len(self.__imagesSubBlocks) > 50
-        index = 0
-        #for image in self.__subblocks:
-         #   image.data()
-          #  index += 1
-           # print(index)
-        print(self.__cziObj.shape)
-        plt.imshow(self.__cziObj.asarray()[0, 0, 0, :, :, 0])
-        plt.show()
+        out, self.__channels = cziUtil.decodeImages(self.__cziObj, max_workers=4)
+        self.__fullImageMosaic = np.squeeze(out).astype(np.float32)
+        self.__imageMosaics = self.__fullImageMosaic
+        if self.__fullImageMosaic.ndim == 3:
+            self.__imageMosaics = [Channel(self.__fullImageMosaic[x, :, :]) for x in range(3)]
+
+    def getChannel(self, index: int) -> Channel:
+        return self.__channels[index]
+
+    def getImageMosaicAtChannel(self, channel: int):
+        return self.__imageMosaics[channel]
+
+    def __len__(self) -> int:
+        return len(self.__channels)
+
+    def __getitem__(self, item) -> Channel:
+        return self.getChannel(item)
 
     def __del__(self):
-        cziUtil.closeCziFileObject(self.__cziObj)
+        if self.__fileOpened:
+            print("Closing")
+            cziUtil.closeCziFileObject(self.__cziObj)
 
 
 class _ImageFromTIFFFile:
@@ -47,8 +64,14 @@ class _ImageFromTIFFFile:
 
     def __init__(self, path: str):
         tiffFileObject = self.tifffile.TiffFile(path)
-        imagesAsArray = tiffFileObject.asarray().astype(dtype="float32")
+        series = tiffFileObject.series
+        self.__channel = []
+        for iterator in series:
+            self.__channel.append(Channel(iterator.asarray().astype(np.float32)))
         self.__metadata = tiffFileObject.ome_metadata
+
+    def getChannel(self, index: int) -> Channel:
+        return self.__channel[index]
 
 
 class _ImageFromOtherFile:
@@ -59,6 +82,11 @@ class _ImageFromOtherFile:
         imageToArray = np.array(image, dtype=np.float32)
 
 
+
 if __name__ == '__main__':
-    path = r"A:\injection AAV\résultats bruts\AAV\AAV498AAV455\AAV498AAV455_S95\AAV498-455_S95_C-07.czi"
+    path = r"A:\injection AAV\résultats bruts\AAV\AAV493AAV498\AAV493AAV498_S51\AAV493AAV498_S51\S51-06.czi"
+    path2 = r"A:\injection AAV\résultats bruts\AAV\AAV498AAV455\AAV498AAV455_S95\AAV498-455_S95_C-06.czi"
+    path3 = r"A:\injection AAV\résultats bruts\AAV\AAV343\Jun109_AAV344a.tif"
+    path4 = r"AAV498-455_S95_C-06.czi"
+    path5 = r"S51-06.czi"
     im = Image(path)
