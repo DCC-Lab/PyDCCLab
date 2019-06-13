28c1ebad1dede7650ec8f39b37cb9c9cf41130c8
Ludovick Bégin <ludovick.begin@gmail.com>
2019-06-13 19:29:19 -0400

    started some tests for ZStack... + Issue/comments:
    It still feels weird to be handling a collection of Image objects while I have a 3D array ready that could be enough on its own. Since I decided to keep ZStack as a child of ImageCollection, i'm cutting the array and loading all layers as 2D image objects... Ok but it looks unecessary and probably time-consuming.
    
    Would be nice to test ImageCollection all the way. I see there's a DCCImageCollection test file but it looks deprecated.

---
+++ dcclab/tests/testZStack.py
@@ -0,0 +1,39 @@
+from dcclab.imageCollection import ZStack, ImageCollection
+import numpy as np
+import unittest
+
+
+class TestZStack(unittest.TestCase):
+
+    def setUp(self):
+        self.depth = 5
+        self.grayStack = np.zeros((10, 10, self.depth))
+
+    def testImageCollectionFrom3DArray(self):  # fixme: sorry for touching another class. Image & ImageCollection tests look deprecated
+        collection = ImageCollection(self.grayStack)
+
+        self.assertTrue(len(collection) == self.depth)
+        self.assertTrue(collection[0].shape == (10, 10))
+        self.assertTrue(collection[0].asArray().shape == (10, 10))
+
+    def testZStackFrom3DArray(self):
+        stack = ZStack(self.grayStack)
+        self.assertTrue(len(stack) == self.depth)
+        self.assertTrue(stack[0].shape == (10, 10))
+        self.assertTrue(stack[0].asArray().shape == (10, 10))
+
+    def testZStackShape(self):
+        stack = ZStack(self.grayStack)
+        self.assertTrue(stack.shape == (10, 10, 5))
+
+    def testZStackAsArray(self):
+        stack = ZStack(self.grayStack)
+        stackArray = stack.asArray()
+        self.assertIsInstance(stackArray, np.ndarray)
+        self.assertTrue(stackArray.shape == (10, 10, 5))
+
+    @unittest.skip("Collection from 4D arrays is Not Implemented")
+    def testZStackFromRGBArray(self):
+        RGBStack = np.zeros((40, 40, 10, 3))
+
+    # Todo: test zStack/ImageCollection from image files
