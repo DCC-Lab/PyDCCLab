974f9b229f124bb734fbeb59f9e770a96a3ff72a
Ludovick Bégin <ludovick.begin@gmail.com>
2019-06-14 17:48:58 -0400

    zStack test mask and label

--- dcclab/tests/testZStack.py
+++ dcclab/tests/testZStack.py
@@ -33,44 +33,84 @@ class TestZStackFrom3DArray(unittest.TestCase):
 
     def testArray(self):
         stackArray = self.zStack.array
+
         self.assertIsInstance(stackArray, np.ndarray)
         self.assertTrue(stackArray.shape == (10, 10, 5))
 
     def testRemoveNoise(self):  # fixme ?: not testing scipy methods individually
         self.zStack.removeNoise()
+
         self.assertTrue(self.zStack.array.mean() != self.grayStack.mean())
 
     def testRemoveNoiseKeepOriginal(self):
         originalStack = self.zStack.array.copy()
         self.zStack.removeNoise()
+
         self.assertIsInstance(self.zStack.originalZStack, np.ndarray)
         self.assertTrue(np.array_equal(self.zStack.originalZStack, originalStack))
 
     def testRemoveNoiseDoNotKeepOriginal(self):
         zStack = ZStack(self.grayStack, keepOriginal=False)
         zStack.removeNoise()
+
         self.assertIsNone(zStack.originalZStack)
 
-    def testMask(self):
-        pass
-
     def testSetMaskCreatesCopy(self):
-        pass
+        self.zStack.setMask()
 
-    def testApplyMask(self):
-        pass
+        self.assertIsInstance(self.zStack.maskedZStack, np.ndarray)
 
-    def testLabelWithoutMask(self):
-        pass
+    def testSetMaskDontSetOriginal(self):
+        self.zStack.setMask()
 
-    def testLabel(self):
-        pass
+        self.assertIsNone(self.zStack.originalZStack)
+        self.assertIsInstance(self.zStack.maskedZStack, np.ndarray)
 
-    def testSetLabelCreatesCopy(self):
-        pass
+    def testSetMask(self):  # fixme ?: not testing scipy methods individually
+        self.zStack.setMask(maskClosing=1)
+        trueRegion = np.ones((6, 6, self.depth-2)) == 1
+
+        self.assertEqual(self.zStack.maskedZStack.dtype, np.dtype('bool'))
+        self.assertTrue(np.array_equal(self.zStack.maskedZStack[2:8, 2:8, 1:self.depth-1], trueRegion))
+
+    def testApplyMaskKeepOriginal(self):
+        zStackBackup = self.zStack.array.copy()
+        self.zStack.applyMask(maskClosing=1)
+
+        self.assertTrue(np.array_equal(self.zStack.originalZStack, zStackBackup))
+
+    def testSetLabel(self):
+        self.zStack.setMask(maskClosing=1)
+        self.zStack.setLabel()
+        onesRegion = np.ones((6, 6, self.depth-2))
+
+        self.assertEqual(self.zStack.labeledZStack.dtype, np.dtype('int'))
+        self.assertTrue(np.array_equal(self.zStack.labeledZStack[2:8, 2:8, 1:self.depth-1], onesRegion))
+
+    def testSetLabelWithoutMask(self):
+        with self.assertRaises(Exception):
+            self.zStack.setLabel()
+
+    def testSetLabelDontSetOriginal(self):
+        self.zStack.setMask()
+        self.zStack.setLabel()
+
+        self.assertIsNone(self.zStack.originalZStack)
+        self.assertIsInstance(self.zStack.labeledZStack, np.ndarray)
+
+    def testSetLabelWithAppliedMask(self):
+        self.zStack.applyMask()
+        self.zStack.setLabel()
+
+        self.assertIsInstance(self.zStack.originalZStack, np.ndarray)
+        self.assertIsInstance(self.zStack.labeledZStack, np.ndarray)
 
     def testApplyLabel(self):
-        pass
+        self.zStack.setMask()
+        self.zStack.applyLabel()
+
+        self.assertIsInstance(self.zStack.originalZStack, np.ndarray)
+        self.assertIsNone(self.zStack.labeledZStack)
 
     def testAllStacksAreInMemory(self):
         pass
