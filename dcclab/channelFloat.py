b7d278ec965142aeb4a75d99188aa4a89460c50d
Gabriel Genest <gabriel.genest.2@ulaval.ca>
2019-06-14 10:38:36 -0400

    Modified methods of conversion to include the new genreal method of conversion to unsigned int and added the implementation of the getHistogramValues method

--- dcclab/channelFloat.py
+++ dcclab/channelFloat.py
@@ -15,16 +15,25 @@ class ChannelFloat(Channel):
             normalizedPixels = np.copy(pixels) / maxValue
         Channel.__init__(self, normalizedPixels)
 
+    def getHistogramValues(self, normed: bool = False) -> typing.Tuple[np.ndarray, np.ndarray]:
+        pixels = self.convertTo8BitsInteger().pixels
+        array = pixels.ravel()
+        nbBins = len(np.bincount(array))
+        hist, bins = np.histogram(array, nbBins, [0, nbBins], density=normed)
+        return hist, bins
+
     def convertTo8BitsInteger(self):
-        newPixels = (np.copy(self.pixels) * 255).astype(np.uint8)
-        return Channel(newPixels.astype(np.uint8))
+        return self.convertToUnsignedInt(np.uint8)
 
     def convertTo16BitsInteger(self):
-        newPixels = ((np.copy(self.pixels)) * (2 ** 16 - 1)).astype(np.uint16)
-        return Channel(newPixels.astype(np.uint16))
+        return self.convertToUnsignedInt(np.uint16)
 
     def getEntropyFiltering(self, filterSize: int):
         # We have to cast image in 8 bits uint because the algorithm seems to properly work only in this type
         pixels = self.convertTo8BitsInteger().pixels
         entropyFiltered = entropy(pixels, morphology.selem.square(filterSize, dtype=np.float32))
         return Channel(entropyFiltered.astype(np.float32))
+
+    def convertToUnsignedInt(self, dtype):
+        convertedArray = ((np.copy(self.pixels)) * np.iinfo(dtype).max)
+        return Channel(convertedArray.astype(dtype))
