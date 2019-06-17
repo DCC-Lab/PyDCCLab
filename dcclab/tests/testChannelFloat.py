eb89d1847efaebd29fc808f5c7c1219f6c2f64d6
Gabriel Genest <gabriel.genest.2@ulaval.ca>
2019-06-17 13:30:44 -0400

    Create testChannelFloat.py
    New test file and test cases for ChannelFloat

---
+++ dcclab/tests/testChannelFloat.py
@@ -0,0 +1,27 @@
+import env
+from dcclab import *
+import unittest
+import numpy as np
+
+
+class TestChannelFloat(unittest.TestCase):
+
+    def setUp(self) -> None:
+        array = np.ones((10, 10), dtype=np.float32) * 2.35
+        self.channelNotNormalized = Channel(array)
+        arrayNormed = np.ones_like(array) * 0.87
+        self.channelNormalized = Channel(arrayNormed)
+
+    def testValidConstructor(self):
+        self.assertTrue(
+            isinstance(self.channelNormalized, ChannelFloat) and isinstance(self.channelNotNormalized, ChannelFloat))
+
+    def testIsNormalizedAfterInit(self):
+        self.assertTrue(np.max(self.channelNotNormalized.pixels) == 1)
+
+    def testGetHistogramValues(self):
+
+
+
+if __name__ == '__main__':
+    unittest.main()
