76d37d11169bebd6d82cfd805f507061951037fc
Gabriel Genest <gabriel.genest.2@ulaval.ca>
2019-06-20 11:13:36 -0400

    Modified the usage test file to also compute the standard deviation
    I also modified the error handling when running the script:  If the file contains multiple images (zstack, time serie, etc) the result file will contain "NotImplemented" as average and stdDev values. Sometimes, the metadata is wrong : the file only has one channel (one image, the "C" dimension is only 1) but the metadata specifies that there are more than one channel. When trying to access the invalid channel index, IndexError are raised. The script now handle them and write "IndexError" as average and stdDev values.

--- dcclab/tests/testDatabaseQueryAndAverageExtraction.py
+++ dcclab/tests/testDatabaseQueryAndAverageExtraction.py
@@ -1,45 +1,58 @@
 import env
-from dcclab import Image
+from dcclab import Image, cziUtil
 import os
 
 # For this test, the query was done by Mathieu on his computer. Selected channel is mCherry
 if __name__ == '__main__':
     listOfAverages = []
+    listOfStdDev = []
 
     readQuery = open(r"C:\Users\goubi\PycharmProjects\BigData-ImageAnalysis\dcclab\tests\querry_mcher.csv", "r",
                      encoding="utf-8")
-    writeResults = open(r"C:\Users\goubi\PycharmProjects\BigData-ImageAnalysis\dcclab\tests\querry_mcher_results.czv",
-                        "w", encoding="utf-8")
-    writeResults.write(
-        "WARNING: The czi file reader is only capable of properly reading single image (mutli or mono channel). "
-        "If it is a z-stack or a time serie, only one image will be taken.")
+    writeResults = open(r"C:\Users\goubi\PycharmProjects\BigData-ImageAnalysis\dcclab\tests\querry_mcher_results.csv",
+                        "a", encoding="utf-8")
 
     lines = readQuery.readlines()
     numberOfFiles = len(lines)
-    currentNumber = 0
-    for line in lines:
 
-        path, channelNumber = line.split(",")
+    startIndex = 0
+    if startIndex == 0:
+        writeResults.write(
+            "WARNING: The czi file reader is only capable of properly reading single image (mutli or mono channel). "
+            "If it is a z-stack or any other multi image file, it will be ignored.")
+    for i in range(startIndex, len(lines)):
+        path, channelNumber = lines[i].split(",")
         pathChanged = path.replace("P", "A", 1)  # Change the P drive to A for my computer
-        ok = ""
+        ok = True
         size = os.stat(pathChanged).st_size
-        if size > 200 * 1024 * 1024:
-            while ok not in ["Y", "N"]:
-                ok = input("Heavy file ({}). Continue? (Y/N)".format(size)).upper()
-        if ok in ["Y", ""]:
+        if size > 550 * 1024 * 1024:
+            ok = False
+        if ok:
             try:
                 image = Image(path=pathChanged)
                 average = image.channels[int(channelNumber)].getAverageValueOfPixels()
+                stdDev = image.channels[int(channelNumber)].getStandardDeviation()
                 listOfAverages.append(average)
+                listOfStdDev.append(stdDev)
                 print(average)
+                print(stdDev)
             except NotImplementedError as e:
                 print(e)
-                average = "-"
+                average = "NotYetImplemented"
+                stdDev = average
+                print("NotYetImplemented")
+            except IndexError:
+                print("Index error : ", pathChanged, channelNumber, image.shape)
+                average = "IndexError"
+                stdDev = average
         else:
-            average = "-"
-        writeResults.write("{0},{1},{2}".format(path, channelNumber, average))
-        currentNumber += 1
-        print("{} / {} files read".format(currentNumber, numberOfFiles))
+            average = "FileTooBig"
+            stdDev = average
+            print("FileTooBig")
+        writeResults.writelines(
+            "\n{path},{number},{average},{stdDev}".format(path=path, number=channelNumber.strip(), average=average,
+                                                          stdDev=stdDev))
+        print("{} / {} files read".format(i + 1, numberOfFiles))
 
     readQuery.close()
     writeResults.close()
