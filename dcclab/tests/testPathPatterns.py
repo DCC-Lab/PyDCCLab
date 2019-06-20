736d3f211cd0770a36f405aac4efdbbaab89adb3
Daniel Cote <dccote@novajo.ca>
2019-06-19 20:52:34 -0400

    Better with Unix and Windows directory names

--- dcclab/tests/testPathPatterns.py
+++ dcclab/tests/testPathPatterns.py
@@ -1,11 +1,24 @@
 import env
 from dcclab import *
-
 import unittest
 import numpy as np
 import re
+from pathlib import Path, PureWindowsPath
+import tempfile
+from testUtils import *
+import time
+
 
 class TestPatterns(unittest.TestCase):
+    def setUp(self):
+        self.tmpdir = Path("{0}/{1}".format(tempfile.gettempdir(), "images"))
+        self.tmpdir.mkdir( parents=True, exist_ok=True )
+
+    def tearDown(self):
+        for filename in self.tmpdir.iterdir():
+            Path(self.tmpdir / filename).unlink()
+        self.tmpdir.rmdir()
+
 
     def testInit(self):
         self.assertIsNotNone(PathPattern('abc'))
@@ -80,7 +93,7 @@ class TestPatterns(unittest.TestCase):
 
     def testEmptyDirectory(self):
         pat = PathPattern(r'test.tiff')
-        self.assertEqual(pat.directory, "./")
+        self.assertEqual(pat.directory, ".")
 
     def testBasename(self):
         pat = PathPattern(r'/Users/dccote/test.tiff')
@@ -168,6 +181,9 @@ class TestPatterns(unittest.TestCase):
                 for k in range(4):
                     with self.assertRaises(ValueError):
                         pat.filePathWithIndex(i,j,k)
+
+    def testTestDirPresent(self):
+        self.assertTrue(self.tmpdir)
 
 if __name__ == '__main__':
     unittest.main()
