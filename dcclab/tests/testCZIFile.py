29310c1c64395898283967941568eb21bad430be
Gabriel Genest <gabriel.genest.2@ulaval.ca>
2019-06-21 10:05:21 -0400

    New tests cases

--- dcclab/tests/testCZIFile.py
+++ dcclab/tests/testCZIFile.py
@@ -1,14 +1,36 @@
 import env
 import unittest
 from dcclab import CZIFile
+import czifile
+import numpy as np
 
 
 class TestCZIFile(unittest.TestCase):
 
-    def testMultipleScenesError(self):
+    def testAxesNotSupported(self):
         czi = CZIFile("testCziAxesNotYetSupported.czi")
         try:
             czi.imageDataFromPath()
             self.fail("If this is reached, no exception raised!")
         except NotImplementedError as e:
             self.assertEqual(str(e), "BSTCYX0")
+
+    def testMultiSceneCZI(self):
+        czi = CZIFile("testCziMultipleScenes.czi")
+        try:
+            czi.imageDataFromPath()
+            self.fail("If this is reached, no exception raised!")
+        except NotImplementedError as e:
+            self.assertEqual(str(e), "Multiple scenes")
+
+    def testMosaicOutputOneChannel(self):
+        cziObj = czifile.CziFile("testCziOneChannel.czi")
+        array = np.squeeze(cziObj.asarray())
+        cziObj.close()
+        czi = CZIFile("testCziOneChannel.czi")
+        image = czi.imageDataFromPath()
+        self.assertTrue(np.array_equal(array, image))
+
+
+if __name__ == '__main__':
+    unittest.main()
