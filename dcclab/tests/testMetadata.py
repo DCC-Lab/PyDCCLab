744b016b3e485cd2c85f455a62e7ac160c673d59
MFournierQC <50327179+MFournierQC@users.noreply.github.com>
2019-06-26 10:55:37 -0400

    Modification of a test CSV file.

--- dcclab/tests/testMetadata.py
+++ dcclab/tests/testMetadata.py
@@ -5,6 +5,9 @@ import os
 
 class TestMetadata(unittest.TestCase):
     def setUp(self) -> None:
+        self.directory = os.path.dirname(__file__)
+        self.cziPath = os.path.join(self.directory, 'testCziFile.czi')
+        self.csvPath = os.path.join(self.directory, '')
         pass
 
     def tearDown(self) -> None:
