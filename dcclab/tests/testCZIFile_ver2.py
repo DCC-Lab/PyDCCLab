ae872883c0850c2cf7e3fc4a1db5984b0421ae22
Gabriel Genest <gabriel.genest.2@ulaval.ca>
2019-06-26 10:28:20 -0400

    Added tests

--- dcclab/tests/testCZIFile_ver2.py
+++ dcclab/tests/testCZIFile_ver2.py
@@ -24,8 +24,6 @@ class TestConstructor(unittest.TestCase):
 
 class TestProperties(unittest.TestCase):
 
-    def testNumberOfChannels(self):
-        self.assertEqual(CZIFile("testCziFileTwoChannels.czi").numberOfChannels, 2)
 
     def testChannelMapsLength(self):
         self.assertEqual(len(CZIFile("testCziFileTwoChannels.czi").channelMaps), 2)
@@ -34,20 +32,30 @@ class TestProperties(unittest.TestCase):
         czi = CZIFile("testCziFileTwoChannels.czi")
         keysChannel1 = list(czi.channelMaps[0].keys())
         keysChannel2 = list(czi.channelMaps[1].keys())
-        supposedKeys = [(range(0, 1936), range(0, 1460), None, None)]
+        supposedKeys = [(range(0, 1936), range(0, 1460), None, None, None)]
         self.assertTrue(keysChannel1 == supposedKeys and keysChannel2 == supposedKeys)
 
     def testChannelMapsValues(self):
         czi = CZIFile("testTinyCzi.czi")
         pixelsValueChannelOne = np.array([[119, 142], [134, 118], [122, 125]])
         pixelsValueChannelTwo = np.array([[68, 72], [77, 72], [57, 69]])
-        key = (range(0, 2), range(0, 3), None, None)
+        key = (range(0, 2), range(0, 3), None, None, None)
         self.assertTrue(
             np.array_equal(czi.channelMaps[0][key], pixelsValueChannelOne) and np.array_equal(czi.channelMaps[1][key],
                                                                                               pixelsValueChannelTwo))
+
     def testChannelMapsAllSameKeys(self):
-        #todo test if keys from every channel are the same
-        pass
+        czi = CZIFile("testCziFileThreeChannelsFourTiles.czi")
+        channelMaps = czi.channelMaps
+        self.assertTrue(channelMaps[0].keys() == channelMaps[1].keys() == channelMaps[2].keys())
+
+    def testNumberOfChannels(self):
+        czi3Channels = CZIFile("testCziFileThreeChannelsFourTiles.czi")
+        czi2Channels = CZIFile("testCziFileTwoChannels.czi")
+        cziOneChannel = CZIFile("testCziOneChannel.czi")
+        czi0Channel = CZIFile("testCziFileYX0Axes.czi")
+        self.assertTrue(czi3Channels.numberOfChannels == 3 and czi2Channels.numberOfChannels == 2
+                        and cziOneChannel.numberOfChannels == 1 and czi0Channel.numberOfChannels == 0)
 
 
 if __name__ == '__main__':
