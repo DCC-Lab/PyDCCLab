5f8c89f7431028ff3c9f5007d24b6c6c60e8f29b
MFournierQC <50327179+MFournierQC@users.noreply.github.com>
2019-06-26 09:34:57 -0400

    Query for GabGabG

--- dcclab/scratch.py
+++ dcclab/scratch.py
@@ -5,7 +5,14 @@ if __name__ == '__main__':
     database = Database(r'P:\injection AAV\résultats bruts\mtp.db')
     database.connect()
 
+    '''
     select = database.select('cziChannels', 'channel_id', 'channel_name="DAPI"')
     with open('query.csv', 'w', encoding='UTF-8') as file:
         for line in select:
             file.write('{}\n'.format(line['channel_id']))
+    '''
+
+    select = database.select('cziMetadata', 'path', 'channels<1')
+    with open('query.csv', 'w', encoding='UTF-8') as file:
+        for line in select:
+            file.write('{}\n'.format(line['path']))
