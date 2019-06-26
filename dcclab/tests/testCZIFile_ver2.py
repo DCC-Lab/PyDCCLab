01e518d4b25cd9a57fe48948083955f47215afe5
Gabriel Genest <gabriel.genest.2@ulaval.ca>
2019-06-26 12:03:33 -0400

    Added new (complete and incomplete) test cases

--- dcclab/tests/testCZIFile_ver2.py
+++ dcclab/tests/testCZIFile_ver2.py
@@ -24,7 +24,6 @@ class TestConstructor(unittest.TestCase):
 
 class TestProperties(unittest.TestCase):
 
-
     def testChannelMapsLength(self):
         self.assertEqual(len(CZIFile("testCziFileTwoChannels.czi").channelMaps), 2)
 
@@ -56,6 +55,64 @@ class TestProperties(unittest.TestCase):
         czi0Channel = CZIFile("testCziFileYX0Axes.czi")
         self.assertTrue(czi3Channels.numberOfChannels == 3 and czi2Channels.numberOfChannels == 2
                         and cziOneChannel.numberOfChannels == 1 and czi0Channel.numberOfChannels == 0)
+
+    def testTotalWidth(self):
+        czi1936x1460 = CZIFile("testCziFileTwoChannels.czi")
+        czi3790x2892 = CZIFile("testCziFileThreeChannelsFourTiles.czi")
+        czi954x670 = CZIFile("testCziFileYX0Axes.czi")
+        czi2x3 = CZIFile("testTinyCzi.czi")
+        czi1525x905 = CZIFile("testCziThreeChannelsOneScene.czi")
+        self.assertTrue(czi2x3.totalWidth == 2 and czi954x670.totalWidth == 954 and czi1525x905.totalWidth == 1525
+                        and czi1936x1460.totalWidth == 1936 and czi3790x2892.totalWidth == 3790)
+
+    def testTotalHeight(self):
+        czi1936x1460 = CZIFile("testCziFileTwoChannels.czi")
+        czi3790x2892 = CZIFile("testCziFileThreeChannelsFourTiles.czi")
+        czi954x670 = CZIFile("testCziFileYX0Axes.czi")
+        czi2x3 = CZIFile("testTinyCzi.czi")
+        czi1525x905 = CZIFile("testCziThreeChannelsOneScene.czi")
+        self.assertTrue(czi2x3.totalHeight == 3 and czi954x670.totalHeight == 670 and czi1525x905.totalHeight == 905
+                        and czi1936x1460.totalHeight == 1460 and czi3790x2892.totalHeight == 2892)
+
+    def testIsZStackNo(self):
+        czi = CZIFile("testCziThreeChannelsOneScene.czi")
+        self.assertFalse(czi.isZstack)
+
+    def testIsZStackYes(self):
+        # todo find file with zstack, timeserie (for another false)
+        pass
+
+    def testIsTimeSerieNo(self):
+        czi = CZIFile("testCziThreeChannelsOneScene.czi")
+        self.assertFalse(czi.isTimeSerie)
+
+    def testIsTimeSerieYes(self):
+        # todo find file with timeserie, zstack (for another false)
+        pass
+
+    def testIsSceneNo(self):
+        czi = CZIFile("testCziFileYX0Axes.czi")
+        self.assertFalse(czi.isScene)
+
+    def testIsSceneYes(self):
+        czi = CZIFile("testCziThreeChannelsOneScene.czi")
+        self.assertTrue(czi.isScene)
+
+    def testIsSceneNo2(self):
+        # todo find file with timeserie/zstack for another false
+        pass
+
+    def testShape(self):
+        czi1936x1460 = CZIFile("testCziFileTwoChannels.czi")
+        czi3790x2892 = CZIFile("testCziFileThreeChannelsFourTiles.czi")
+        czi954x670 = CZIFile("testCziFileYX0Axes.czi")
+        czi2x3 = CZIFile("testTinyCzi.czi")
+        czi1525x905 = CZIFile("testCziThreeChannelsOneScene.czi")
+        self.assertTupleEqual(czi2x3.shape, (1, 2, 3, 2, 1))
+        self.assertTupleEqual(czi954x670.shape, (670, 954, 3))
+        self.assertTupleEqual(czi1525x905.shape, (1, 1, 3, 905, 1525, 1))
+        self.assertTupleEqual(czi1936x1460.shape, (1, 2, 1460, 1936, 1))
+        self.assertTupleEqual(czi3790x2892.shape, (1, 1, 3, 2892, 3790, 1))
 
 
 if __name__ == '__main__':
