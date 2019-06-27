a362865d9d02658a88525f6cbabd918eceb9b5df
MFournierQC <50327179+MFournierQC@users.noreply.github.com>
2019-06-27 13:08:39 -0400

    Test on cafeine2 server, take 2 electric boogaloo.

--- dcclab/scratch.py
+++ dcclab/scratch.py
@@ -4,7 +4,9 @@ import os
 
 if __name__ == '__main__':
     directory = os.path.dirname(os.path.dirname(__file__))
+    print(directory)
     path = os.path.join(directory, 'mountPom', 'injection AAV', 'résultats bruts', 'mtp.db')
+    print(path)
     #database = Database(r'P:\injection AAV\résultats bruts\mtp.db')
     database = Database(path)
     database.connect()
@@ -16,7 +18,9 @@ if __name__ == '__main__':
             file.write('{}\n'.format(line['channel_id']))
     '''
 
+    database.begin()
     select = database.select('cziMetadata', 'path', 'channels<1')
+    database.end()
     with open('query.csv', 'w', encoding='UTF-8') as file:
         for line in select:
             file.write('{}\n'.format(line['path']))
