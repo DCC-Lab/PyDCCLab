593ab13f6d4be7b64bd1ffe8501b7a5f6c53d70f
Daniel Cote <dccote@novajo.ca>
2019-06-30 02:42:22 -0400

    updated more tests

--- dcclab/__init__.py
+++ dcclab/__init__.py
@@ -9,7 +9,7 @@ from .DCCExceptions import *
 from .channelInteger import *
 from .channelFloat import *
 from .pathPattern import *
-#from .lifFile import *
+from .lifFile import *
 from .cziFile import *
 
 from .database import *
