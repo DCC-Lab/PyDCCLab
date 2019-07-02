0b8a3c72e8fd3e088c972763acd9d28831e8d7a7
Gabriel Genest <gabriel.genest.2@ulaval.ca>
2019-07-02 14:32:58 -0400

    New file to regroup data extraction
    Added entropy, median, min and max

---
+++ dcclab/tests/testDataExtraction.py
@@ -0,0 +1,210 @@
+import env
+from dcclab import *
+import unittest
+
+header = "path,channelNumber,average,stdDev,entropy,median,min,max,averageN,stdDevN,entropyN,medianN,minN,maxN"
+
+
+class DataExtractionMCherry(env.dcclabTestCase):
+    def setUp(self):
+        with open(Path(self.dataDir / r"query_mcher.csv"), "r",
+                  encoding="utf-8") as readQuery:
+            self.lines = readQuery.readlines()
+
+        self.writeResults = open(Path(self.dataDir / r"query_mcher_results.csv"),
+                                 "w", encoding="utf-8")
+        self.writeResults.writelines(header)
+
+    def tearDown(self):
+        self.writeResults.close()
+
+    def testMCherryExtraction(self):
+        numberOfFiles = len(self.lines)
+        for i in range(numberOfFiles):
+            path, channelNumber = self.lines[i].split(";")
+            channelNumber = channelNumber.split(":")[1]
+            pathChanged = os.path.join("../", path)
+            try:
+                image = Image(path=pathChanged)
+                channel = image.channels[int(channelNumber)]
+                average = channel.getAverageValueOfPixels()
+                stdDev = channel.getStandardDeviation()
+                entropy = channel.getShannonEntropyOfPixels
+                median = channel.getMedian()
+                minimum = channel.getMinimum()
+                maximum = channel.getMaximum()
+                normalizedChannel = channel.convertToNormalizedFloat()
+                averageN = normalizedChannel.getAverageValueOfPixels()
+                stdDevN = normalizedChannel.getStandardDeviation()
+                entropyN = normalizedChannel.getShannonEntropyOfPixels
+                medianN = normalizedChannel.getMedian()
+                minimumN = normalizedChannel.getMinimum()
+                maximumN = normalizedChannel.getMaximum()
+
+                print(average)
+                print(stdDev)
+            except Exception as e:
+
+                if isinstance(e, NotImplementedError):
+                    average = "NotYetImplemented"
+                elif isinstance(e, IndexError):
+                    average = "IndexError"
+                else:
+                    average = "OtherError"
+                print(average)
+                stdDev = average
+                entropy = average
+                median = average
+                minimum = average
+                maximum = average
+                averageN = average
+                stdDevN = average
+                entropyN = average
+                medianN = average
+                minimumN = average
+                maximumN = average
+
+            self.writeResults.writelines(
+                "\n{},{},{},{},{},{},{},{},{},{},{},{},{},{}".format(path, channelNumber.strip(), average,
+                                                                     stdDev, entropy, median, minimum, maximum,
+                                                                     averageN,
+                                                                     stdDevN, entropyN, medianN, minimumN, maximumN))
+            print("{} / {} files read (mCherry)".format(i + 1, numberOfFiles))
+
+
+class DataExtractionEGFP(env.dcclabTestCase):
+    def setUp(self):
+        with open(Path(self.dataDir / r"query_egfp.csv"), "r",
+                  encoding="utf-8") as readQuery:
+            self.lines = readQuery.readlines()
+
+        self.writeResults = open(Path(self.dataDir / r"query_egfp_results.csv"),
+                                 "w", encoding="utf-8")
+        self.writeResults.writelines(header)
+
+    def tearDown(self):
+        self.writeResults.close()
+
+    def testEGFPExtraction(self):
+        numberOfFiles = len(self.lines)
+        for i in range(numberOfFiles):
+            path, channelNumber = self.lines[i].split(";")
+            channelNumber = channelNumber.split(":")[1]
+            pathChanged = os.path.join("../", path)
+            try:
+                image = Image(path=pathChanged)
+                channel = image.channels[int(channelNumber)]
+                average = channel.getAverageValueOfPixels()
+                stdDev = channel.getStandardDeviation()
+                entropy = channel.getShannonEntropyOfPixels
+                median = channel.getMedian()
+                minimum = channel.getMinimum()
+                maximum = channel.getMaximum()
+                normalizedChannel = channel.convertToNormalizedFloat()
+                averageN = normalizedChannel.getAverageValueOfPixels()
+                stdDevN = normalizedChannel.getStandardDeviation()
+                entropyN = normalizedChannel.getShannonEntropyOfPixels
+                medianN = normalizedChannel.getMedian()
+                minimumN = normalizedChannel.getMinimum()
+                maximumN = normalizedChannel.getMaximum()
+
+                print(average)
+                print(stdDev)
+            except Exception as e:
+
+                if isinstance(e, NotImplementedError):
+                    average = "NotYetImplemented"
+                elif isinstance(e, IndexError):
+                    average = "IndexError"
+                else:
+                    average = "OtherError"
+                print(average)
+                stdDev = average
+                entropy = average
+                median = average
+                minimum = average
+                maximum = average
+                averageN = average
+                stdDevN = average
+                entropyN = average
+                medianN = average
+                minimumN = average
+                maximumN = average
+
+            self.writeResults.writelines(
+                "\n{},{},{},{},{},{},{},{},{},{},{},{},{},{}".format(path, channelNumber.strip(), average,
+                                                                     stdDev, entropy, median, minimum, maximum,
+                                                                     averageN,
+                                                                     stdDevN, entropyN, medianN, minimumN, maximumN))
+            print("{} / {} files read (EGFP)".format(i + 1, numberOfFiles))
+
+
+class DataExtractionDAPI(env.dcclabTestCase):
+    def setUp(self):
+        with open(Path(self.dataDir / r"query_DAPI.csv"), "r",
+                  encoding="utf-8") as readQuery:
+            self.lines = readQuery.readlines()
+
+        self.writeResults = open(Path(self.dataDir / r"query_DAPI_results.csv"),
+                                 "w", encoding="utf-8")
+        self.writeResults.writelines(header)
+
+    def tearDown(self):
+        self.writeResults.close()
+
+    def testDAPIExtraction(self):
+        numberOfFiles = len(self.lines)
+        for i in range(numberOfFiles):
+            path, channelNumber = self.lines[i].split(";")
+            channelNumber = channelNumber.split(":")[1]
+            pathChanged = os.path.join("../", path)
+            try:
+                image = Image(path=pathChanged)
+                channel = image.channels[int(channelNumber)]
+                average = channel.getAverageValueOfPixels()
+                stdDev = channel.getStandardDeviation()
+                entropy = channel.getShannonEntropyOfPixels
+                median = channel.getMedian()
+                minimum = channel.getMinimum()
+                maximum = channel.getMaximum()
+                normalizedChannel = channel.convertToNormalizedFloat()
+                averageN = normalizedChannel.getAverageValueOfPixels()
+                stdDevN = normalizedChannel.getStandardDeviation()
+                entropyN = normalizedChannel.getShannonEntropyOfPixels
+                medianN = normalizedChannel.getMedian()
+                minimumN = normalizedChannel.getMinimum()
+                maximumN = normalizedChannel.getMaximum()
+
+                print(average)
+                print(stdDev)
+            except Exception as e:
+
+                if isinstance(e, NotImplementedError):
+                    average = "NotYetImplemented"
+                elif isinstance(e, IndexError):
+                    average = "IndexError"
+                else:
+                    average = "OtherError"
+                print(average)
+                stdDev = average
+                entropy = average
+                median = average
+                minimum = average
+                maximum = average
+                averageN = average
+                stdDevN = average
+                entropyN = average
+                medianN = average
+                minimumN = average
+                maximumN = average
+
+            self.writeResults.writelines(
+                "\n{},{},{},{},{},{},{},{},{},{},{},{},{},{}".format(path, channelNumber.strip(), average,
+                                                                     stdDev, entropy, median, minimum, maximum,
+                                                                     averageN,
+                                                                     stdDevN, entropyN, medianN, minimumN, maximumN))
+            print("{} / {} files read (DAPI)".format(i + 1, numberOfFiles))
+
+
+if __name__ == '__main__':
+    unittest.main()
