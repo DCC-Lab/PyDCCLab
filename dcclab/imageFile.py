c9a55b815ce1b87c2d8d6c98b6beec3a60025554
Ludovick Bégin <ludovick.begin@gmail.com>
2019-07-02 13:41:54 -0400

    moved/implemented LifFile as an ImageFile Object

--- dcclab/imageFile.py
+++ dcclab/imageFile.py
@@ -1,8 +1,8 @@
+from .__lifReader import LifReader
+from .imageCollection import *
 from .cziUtil import *
 from .channel import *
-import PIL.Image
 import scipy.io as sio
-
 
 
 class ImageFile(object):
@@ -14,6 +14,12 @@ class ImageFile(object):
         self.path = path
 
     def zStackData(self):
+        """
+        :return: images data as z-stack if possible
+        """
+        return None
+
+    def zStacksData(self):
         """
         :return: images data as z-stack if possible
         """
@@ -154,3 +160,83 @@ class MATLABFile(ImageFile):
                         return np.expand_dims(variable, 2)  # always return 3D
 
         return None
+
+
+class LIFFile(ImageFile):
+    supportedFormats = ['lif']
+
+    def __init__(self, path: str):
+        ImageFile.__init__(self, path)
+        self.__lifObject = LifReader(self.path)
+        self.series = self.__lifObject.getSeries()
+
+    @property
+    def numberOfSeries(self):
+        return len(self.series)
+
+    def __len__(self):
+        return self.numberOfSeries
+
+    def __getitem__(self, indices: Union[int, tuple, list, slice]=None):
+        if indices is None:
+            return self.series
+        if type(indices) is slice:
+            return self.series[indices]
+        elif type(indices) is int:
+            return self.series[indices]
+        elif type(indices) is tuple:
+            indices = list(indices)
+
+        if type(indices) is list:
+            return [self.series[i] for i in indices]
+
+    def keepSeries(self, indices):
+        self.series = self[indices]
+        if type(self.series) is not list:
+            self.series = [self.series]
+
+    def removeAt(self, index: int):
+        self.series.pop(index)
+
+    def zStackData(self, seriesIndex: int=None, channelIndices=None, crop=False) -> ZStack:
+        if seriesIndex is None:
+            assert self.numberOfSeries == 1
+            seriesIndex = 0
+
+        print("... Loading serie")
+        stackArray = self.series[seriesIndex].getStack(channelIndices)
+        print("... Loading ZStack Collection")
+        zStack = ZStack(imagesArray=stackArray, cropAtInit=crop)
+        return zStack
+
+    def zStacksData(self, seriesIndices=None, channelIndices=None, crop=False) -> List[ZStack]:
+        if type(seriesIndices) is int:
+            seriesIndices = [seriesIndices]
+
+        series = self[seriesIndices]
+
+        zStacks = []
+        for i, serie in enumerate(series):
+            print("... Loading serie {}/{}".format(i+1, len(series)))
+            stackArray = serie.getStack(channelIndices)
+            print("... Loading ZStack Collection")
+            zStack = ZStack(imagesArray=stackArray, cropAtInit=crop)
+            zStacks.append(zStack)
+
+        return zStacks
+
+    def imageDataFromPath(self):
+        # todo ?  not sure we need to implement this method. It's not a parent method but I see that all other child classes have this method defined...
+        return None
+
+    def metadata(self, serieIndex: int=None, asJson: bool=False):
+        if serieIndex is None:
+            metadata = []
+            for i, serie in enumerate(self.series):
+                metadata.append({'serie %i' % i: serie.getMetadata()})
+        else:
+            metadata = self.series[serieIndex].getMetadata()
+
+        if asJson:
+            return json.dumps(metadata, indent=4)
+        return metadata
