2cae2fde2476191c2e27becd0744e25df4ebaa13
Gabriel Genest <gabriel.genest.2@ulaval.ca>
2019-07-05 13:25:42 -0400

    Added methods to get median and other data graphs

--- dcclab/tests/testDataGraphs.py
+++ dcclab/tests/testDataGraphs.py
@@ -10,9 +10,10 @@ import seaborn as sns
 class DataGraphs(env.DCCLabTestCase):
 
     def setUp(self) -> None:
-        self.mCherry = pd.read_csv(Path(self.dataDir / "query_mcher_results.csv")).dropna(axis="columns")
-        self.dapi = pd.read_csv(Path(self.dataDir / "query_DAPI_results.csv")).dropna(axis="columns")
+        self.mCherry = pd.read_csv(Path(self.dataDir / "query_mcher_results.csv"), sep=";").dropna(axis="columns")
+        self.dapi = pd.read_csv(Path(self.dataDir / "query_DAPI_results.csv"), sep=";").dropna(axis="columns")
         self.egfp = pd.read_csv(Path(self.dataDir / "query_egfp_results.csv")).dropna(axis="columns")
+        print(self.egfp["averageN"])
         self.mCherry = self.mCherry[
             (self.mCherry["average"] != "NotYetImplemented") & (self.mCherry["average"] != "IndexError")]
         self.dapi = self.dapi[
@@ -27,12 +28,6 @@ class DataGraphs(env.DCCLabTestCase):
             self.dapi["medianN"]
         with self.assertRaises(Exception):
             self.egfp["median"]
-        with self.assertRaises(Exception):
-            self.egfp["medianN"]
-        with self.assertRaises(Exception):
-            self.mCherry["median"]
-        with self.assertRaises(Exception):
-            self.mCherry["medianN"]
 
     def testAllChannelsAvgVsStdDev(self):
         avgMC = np.array(self.mCherry["average"], float)
@@ -145,6 +140,63 @@ class DataGraphs(env.DCCLabTestCase):
         plt.ylabel("Entropy")
         plt.xlabel("Standard deviation")
         plt.title("Standard deviation and entropy of images from the POM platform (normalized images)")
+        plt.legend()
+        plt.show()
+
+    def testAllChannelsStdDevVsMedianN(self):
+        stdDevMC = np.array(self.mCherry["stdDevN"], float)
+        medianMC = np.array(self.mCherry["medianN"], float)
+        stdDevDAPI = np.array(self.dapi["stdDevN"], float)
+        medianDAPI = np.array(self.dapi["medianN"], float)
+        stdDevEGFP = np.array(self.egfp["stdDevN"], float)
+        medianEGFP = np.array(self.egfp["medianN"], float)
+        allStdDev = np.concatenate((stdDevMC, stdDevDAPI, stdDevEGFP))
+        allMedian = np.concatenate((medianMC, medianDAPI, medianEGFP))
+        jp = sns.jointplot(allStdDev, allMedian, kind="reg", scatter=False, fit_reg=False)
+        jp.ax_joint.plot(stdDevMC, medianMC, "o", color="red", alpha=0.2, label="mCherry")
+        jp.ax_joint.plot(stdDevDAPI, medianDAPI, "x", color="blue", alpha=0.2, label="DAPI")
+        jp.ax_joint.plot(stdDevEGFP, medianEGFP, "v", color="green", alpha=0.2, label="EGFP")
+        plt.ylabel("Median")
+        plt.xlabel("Standard deviation")
+        plt.title("Standard deviation and median of images from the POM platform (normalized images)")
+        plt.legend()
+        plt.show()
+
+    def testAllChannelsAverageVsMedianN(self):
+        averageMC = np.array(self.mCherry["averageN"], float)
+        medianMC = np.array(self.mCherry["medianN"], float)
+        averageDAPI = np.array(self.dapi["averageN"], float)
+        medianDAPI = np.array(self.dapi["medianN"], float)
+        averageEGFP = np.array(self.egfp["averageN"], float)
+        medianEGFP = np.array(self.egfp["medianN"], float)
+        allAverage = np.concatenate((averageMC, averageDAPI, averageEGFP))
+        allMedian = np.concatenate((medianMC, medianDAPI, medianEGFP))
+        jp = sns.jointplot(allAverage, allMedian, kind="reg", scatter=False, fit_reg=False)
+        jp.ax_joint.plot(averageMC, medianMC, "o", color="red", alpha=0.2, label="mCherry")
+        jp.ax_joint.plot(averageDAPI, medianDAPI, "x", color="blue", alpha=0.2, label="DAPI")
+        jp.ax_joint.plot(averageEGFP, medianEGFP, "v", color="green", alpha=0.2, label="EGFP")
+        plt.ylabel("Median")
+        plt.xlabel("Average")
+        plt.title("Average and median of images from the POM platform (normalized images)")
+        plt.legend()
+        plt.show()
+
+    def testAllChannelsEntropyVsMedianN(self):
+        entropyMC = np.array(self.mCherry["entropyN"], float)
+        medianMC = np.array(self.mCherry["medianN"], float)
+        entropyDAPI = np.array(self.dapi["entropyN"], float)
+        medianDAPI = np.array(self.dapi["medianN"], float)
+        entropyEGFP = np.array(self.egfp["entropyN"], float)
+        medianEGFP = np.array(self.egfp["medianN"], float)
+        allEntropy = np.concatenate((entropyMC, entropyDAPI, entropyEGFP))
+        allMedian = np.concatenate((medianMC, medianDAPI, medianEGFP))
+        jp = sns.jointplot(allEntropy, allMedian, kind="reg", scatter=False, fit_reg=False)
+        jp.ax_joint.plot(entropyMC, medianMC, "o", color="red", alpha=0.2, label="mCherry")
+        jp.ax_joint.plot(entropyDAPI, medianDAPI, "x", color="blue", alpha=0.2, label="DAPI")
+        jp.ax_joint.plot(entropyEGFP, medianEGFP, "v", color="green", alpha=0.2, label="EGFP")
+        plt.ylabel("Median")
+        plt.xlabel("Entropy")
+        plt.title("Entropy and median of images from the POM platform (normalized images)")
         plt.legend()
         plt.show()
 
