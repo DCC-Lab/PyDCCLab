6aaeeed89588cca0e24fb569a199e2a2878477d7
MFournierQC <50327179+MFournierQC@users.noreply.github.com>
2019-07-09 13:47:14 -0400

    Test to databaseUtilities.

--- dcclab/database/databaseUtilities.py
+++ dcclab/database/databaseUtilities.py
@@ -1,10 +1,12 @@
 from zipfile import ZipFile
-from pathlib import Path
 from fnmatch import fnmatch
 import os
 
 
 def findFiles(directory, extension) -> list:
+    # Although os.walk is slow, I haven't found a faster way to find files in a directory and sub directories.
+    # In python 3.x, os.walk was modified to use os.scandir, which greatly improved its performances. I doubt
+    # there is a faster way to do this.
     filesFound = []
     for root, directories, files in os.walk(os.path.normpath(directory)):
         for file in files:
--- dcclab/tests/testDatabaseUtilities.py
+++ dcclab/tests/testDatabaseUtilities.py
@@ -16,27 +16,18 @@ class TestDatabaseUtilities(env.DCCLabTestCase):
     def testFnmatch(self):
         self.assertTrue(fnmatch.fnmatch('test.tar.gz', '*.tar.gz'))
 
-    @unittest.skipIf(os.name == 'posix', reason='Fails on posix.')
+    #@unittest.skipIf(os.name == 'posix', reason='Fails on posix.')
     def testFnmatchUppercase(self):
         self.assertTrue(fnmatch.fnmatch('test.TXT', '*.[tT][xX][tT]'))
 
-    @unittest.skipIf(os.name == 'posix', reason='Fails on posix.')
+    #@unittest.skipIf(os.name == 'posix', reason='Fails on posix.')
     def testFnmatchUpperAndLowerCase(self):
         self.assertTrue(fnmatch.fnmatch('test.ThIsIsAtEsT', '*.thisisatest'))
 
     def testFindFiles(self):
-        dir = os.path.join(self.moduleDir, 'dcclab', 'POM', 'injection AAV')
-        file = open('testWalkAndRecursive.txt', 'w')
-        file.write('>>>>>>>>>BEGIN TESt\n')
-        for i in range(5):
-            file.write('>>>>BEGIN SEARCH (OS WALK)\n')
-            begin = time.perf_counter()
-            listOfFiles = findFiles(dir, '*.czi')
-            file.write('{} files found.'.format(len(listOfFiles)))
-            file.write('>>>>END SEARCH (OS WALK)\n')
-            file.write('Took {}\n'.format(time.perf_counter() - begin))
-        file.write('>>>>>>>>>END TEST\n')
-        file.close()
+        dir = os.path.join(self.moduleDir, 'dcclab', 'database')
+        self.assertTrue(findFiles(dir, '*.py'))
+        self.assertFalse(findFiles(dir, '*.czi'))
 
 
 if __name__ == '__main__':
