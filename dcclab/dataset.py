27c6d2fcdc7c7c8af77678363c22fb9d8fce6b3d
Ludovick Bégin <ludovick.begin@gmail.com>
2019-07-09 15:07:54 -0400

    ML Dataset ideas : needs revision

--- dcclab/dataset.py
+++ dcclab/dataset.py
@@ -3,14 +3,99 @@ from typing import Union, List
 import numpy as np
 
 
-class Dataset(ImageCollection):
-    def __init__(self, images: List['Image']=None, imagesArray: np.ndarray=None, pathPattern: str=None):
+"""
+Machine Learning Dataset
 
-        """ todo: has to load an image folder possibly containing sub folders for different classes
-            and sub folders for labels or image tagged as label... and possibly load a specified label folder ?
-        """
+It needs to be able to load such data structures: 
 
-        super().__init__(images, imagesArray, pathPattern)
+data/
+|-- type_A
+    |-- 0.png
+    |-- ...
+|-- type_A_labels
+    |-- 0.png
+    |-- ...
+|-- type_B
+|-- type_B_labels
+|-- mixed
+|-- mixed_labels
+|-- ...
+
+OR 
+
+data/
+|-- images
+    |-- 0.png  # [each image might contain multiple classes]
+    |-- ...
+|-- labels
+    |-- 0.png
+    |-- ...
 
 
-# todo: check images have same shape
++ some variants considering the fact that the labels might not exist yet.
+
+
+In the first case it has to seperate and remember each type (or source?) by their name.
+This looks to me like each type (called class in ML classification) or source with its labels is one image collection object. 
+This means that a Dataset Class has to load multiple ImageCollections (and remember their names).
+
+I would then define a Dataset class without heritage, that intentiate different ImageCollection objects (or maybe a new `MLCollection` object heriting from ImageCollection).
+"""
+
+
+class Dataset:
+    def __init__(self, directory: str):
+        self.directory = directory
+        self.collections = dict()
+
+        self.loadCollections()
+
+        self.report()
+        
+    def loadCollections(self):
+        # check inside input directory and search for possible subfolders and labels
+        collections = {'nametag1': [['Images'], ['Labels']]}
+
+        # ultimately fills the collections with a nametag, images and labels
+        for nametag, images, labels in collections:
+            self.collections['nametag'] = ImageCollection(images=images)
+            if labels is not None:
+                self.collections['nametag'].loadLabels(labels)
+
+    def report(self):
+        # - images have same shape
+        # - image classes
+        # - class balance
+        # - ...
+        pass
+
+
+"""
+
+Maybe replace ImageCollection with a possible ML Collection ? ...
+
+"""
+
+
+class MLCollection:
+    supportedTypes = ["Image", "Spectra"]
+
+    def __new__(cls, *args, **kwargs):
+        pass
+
+    def augment(self):
+        pass
+
+
+class MLImageCollection(ImageCollection):  # ?
+
+    def augment(self):
+        # Keras image augmentation generator
+        pass
+
+
+class MLSpectraCollection('SpectraCollection'):  # ?
+
+    def augment(self):
+        # Spectra augmentation technique
+        pass
