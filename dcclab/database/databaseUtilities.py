f5dbf2b3ec2de97bd5d3fd37e6ab04f038bd6e84
MFournierQC <50327179+MFournierQC@users.noreply.github.com>
2019-07-09 13:19:12 -0400

    Modifications to database.

--- dcclab/database/databaseUtilities.py
+++ dcclab/database/databaseUtilities.py
@@ -4,24 +4,13 @@ from fnmatch import fnmatch
 import os
 
 
-def findFilesOS(directory, extension) -> list:
+def findFiles(directory, extension) -> list:
     filesFound = []
     for root, directories, files in os.walk(os.path.normpath(directory)):
         for file in files:
             if fnmatch(file, extension):
                 filesFound.append(os.path.join(root, file))
     return filesFound
-
-def findFiles(directory, extension) -> list:
-    files = []
-    subfiles = []
-    for entry in os.scandir(directory):
-        if entry.is_file() and fnmatch(entry, extension):
-            files.append(entry)
-        elif entry.is_dir():
-            subfiles = findFiles(entry, extension)
-    return files + subfiles
-
 
 def appendToZip(path, file):
     try:
--- dcclab/tests/testDatabaseUtilities.py
+++ dcclab/tests/testDatabaseUtilities.py
@@ -1,4 +1,4 @@
-from dcclab import findFilesOS, appendToZip, findFolderInPath, findFiles
+from dcclab import appendToZip, findFolderInPath, findFiles
 import os
 import time
 import fnmatch
@@ -16,9 +16,11 @@ class TestDatabaseUtilities(env.DCCLabTestCase):
     def testFnmatch(self):
         self.assertTrue(fnmatch.fnmatch('test.tar.gz', '*.tar.gz'))
 
+    @unittest.skipIf(os.name == 'posix', reason='Fails on posix.')
     def testFnmatchUppercase(self):
-        self.assertTrue(fnmatch.fnmatch('test.TXT', '*.txt'))
+        self.assertTrue(fnmatch.fnmatch('test.TXT', '*.[tT][xX][tT]'))
 
+    @unittest.skipIf(os.name == 'posix', reason='Fails on posix.')
     def testFnmatchUpperAndLowerCase(self):
         self.assertTrue(fnmatch.fnmatch('test.ThIsIsAtEsT', '*.thisisatest'))
 
@@ -29,17 +31,9 @@ class TestDatabaseUtilities(env.DCCLabTestCase):
         for i in range(5):
             file.write('>>>>BEGIN SEARCH (OS WALK)\n')
             begin = time.perf_counter()
-            listOfFiles = findFilesOS(dir, '*.czi')
-            file.write('{} files found.'.format(len(listOfFiles)))
-            file.write('>>>>END SEARCH (OS WALK)\n')
-            file.write('Took {}\n'.format(time.perf_counter() - begin))
-
-        for i in range(5):
-            file.write('>>>>BEGIN SEARCH (RECURSIVE)\n')
-            begin = time.perf_counter()
             listOfFiles = findFiles(dir, '*.czi')
             file.write('{} files found.'.format(len(listOfFiles)))
-            file.write('>>>>END SEARCH (RECURSIVE)\n')
+            file.write('>>>>END SEARCH (OS WALK)\n')
             file.write('Took {}\n'.format(time.perf_counter() - begin))
         file.write('>>>>>>>>>END TEST\n')
         file.close()
