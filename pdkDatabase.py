56e169b213d39d34ba7b17c0df1a4232d15c96d7
MFournierQC <50327179+MFournierQC@users.noreply.github.com>
2019-07-17 14:43:43 -0400

    Reading xlsx files and finding .ini files.

--- pdkDatabase.py
+++ pdkDatabase.py
@@ -5,7 +5,6 @@ import re
 
 if __name__ == '__main__':
     # Reading the xlsx file
-    '''
     xlsx = 'K:\\Calcium_imaging_file_info.xlsx'
     with xlrd.open_workbook(xlsx) as file:
         sheet = file.sheet_by_index(0)
@@ -16,7 +15,6 @@ if __name__ == '__main__':
                 line += '{}|'.format(sheet.cell_value(row, col))
             line = line.replace(' ', '')
             print(line.rstrip('|'))
-    '''
 
     # Reading the ini files.
     directory = 'K:\\'
@@ -24,11 +22,11 @@ if __name__ == '__main__':
     iniFiles = []
 
     for rawFile in rawFiles:
-        iniFiles.append(re.sub('\.lineshifted\.raw|.raw', '.ini', rawFile))
+        iniFiles.append(re.sub('\.lineshifted\.raw|.raw', '.ini', rawFile, re.IGNORECASE))
 
     for iniFile in iniFiles:
         try:
             with open(iniFile, 'r') as file:
-                print(file.readline())
+                print(file.readlines())
         except:
             print(">>>>{} file doesn't work.".format(iniFile))
