4c42884d2a5ee230f7f9b17196c6ee70ebebcaca
MFournierQC <50327179+MFournierQC@users.noreply.github.com>
2019-07-18 10:31:23 -0400

    Workbook and worksheets.

--- dcclab/metadata/pdkMetadata/xlsxMetadata.py
+++ dcclab/metadata/pdkMetadata/xlsxMetadata.py
@@ -7,16 +7,22 @@ class XLSXMetadata:
     def __init__(self, xlsxPath):
         self.path = xlsxPath
         self.name = self.fileName()
-        self.workbook = self.workbook()
+        self.workbook = self.getWorkbook()
 
-    def workbook(self):
+    def getWorkbook(self) -> xlrd.book:
         try:
             return xlrd.open_workbook(self.path)
         except:
             raise
 
     def worksheets(self):
-        pass
+        worksheets = []
+        try:
+            for sheet in range(self.workbook.nsheets):
+                worksheets.append(self.workbook.sheet_by_index(sheet))
+            return worksheets
+        except:
+            return worksheets
 
     def fileName(self):
         file = os.path.basename(self.path)
