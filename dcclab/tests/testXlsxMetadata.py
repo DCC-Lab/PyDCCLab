169384697fda278ee44568b87eb243b6c0eb01b8
MFournierQC <50327179+MFournierQC@users.noreply.github.com>
2019-07-18 11:29:38 -0400

    Quick tests of some basics functions and setup/teardown

---
+++ dcclab/tests/testXlsxMetadata.py
@@ -0,0 +1,33 @@
+from dcclab import XLSXMetadata as mtdt
+import xlrd
+import xlwt
+import env
+import unittest
+import os
+
+
+class TestXlsxMetadata(env.DCCLabTestCase):
+    def setUp(self) -> None:
+        self.filePath = os.path.join(str(self.dataDir), 'unittest.xlsx')
+        workbook = xlwt.Workbook()
+
+        sheet = workbook.add_sheet('test_1')
+        sheet.write(0, 0, 'test_column_1')
+        sheet.write(0, 1, 'test_column_2')
+        sheet.write(0, 2, 'file_path')
+        sheet = workbook.add_sheet('test_2')
+        sheet.write(0, 0, 'test_id_1')
+        sheet.write(0, 1, 'file_path')
+
+        workbook.save(self.filePath)
+
+    def tearDown(self) -> None:
+        os.remove(self.filePath)
+
+    def testFilename(self):
+        metadata = mtdt(self.filePath)
+        self.assertEqual('unittest', metadata.fileName())
+
+    def testGetWorkbook(self):
+        metadata = mtdt(self.filePath)
+        self.assertTrue(type(metadata.workbook) == xlrd.book.Book)
