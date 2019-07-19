24a1ee4c97314455eba1b5622a8f49a90dd629d7
MFournierQC <50327179+MFournierQC@users.noreply.github.com>
2019-07-19 13:19:57 -0400

    Lineshifted tests.

--- dcclab/tests/testRawMetadata.py
+++ dcclab/tests/testRawMetadata.py
@@ -9,11 +9,20 @@ class TestRawMetadata(env.DCCLabTestCase):
         self.rawPath = os.path.join(str(self.dataDir), '20190101_12_12_12_900nm_16x_512x1024_1000f_8dpf_XYT.raw')
         self.iniPath = os.path.join(str(self.dataDir), '20190101_12_12_12_900nm_16x_512x1024_1000f_8dpf_XYT.ini')
         self.xmlPath = os.path.join(str(self.dataDir), '20190101_12_12_12_900nm_16x_512x1024_1000f_8dpf_XYT.xml')
+        with open(self.iniPath, 'w') as file:
+            file.write('[]\n')
+            file.write('Test File\n')
+
+        with open(self.xmlPath, 'w') as file:
+            file.write('<TEST>\n')
+            file.write('\t<items>\n')
+            file.write('\t</items>\n')
+            file.write('</TEST>\n')
 
     def tearDown(self) -> None:
         #os.remove(self.rawPath)
-        #os.remove(self.iniPath)
-        #os.remove(self.xmlPath)
+        os.remove(self.iniPath)
+        os.remove(self.xmlPath)
         pass
 
     def testFileName(self):
@@ -24,8 +33,20 @@ class TestRawMetadata(env.DCCLabTestCase):
         metadata = mtdt(self.rawPath)
         self.assertEqual(metadata.iniPath, self.iniPath)
 
+    def testIniPathLineShifted(self):
+        filePath = os.path.join(str(self.dataDir),
+                                '20190101_12_12_12_900nm_16x_512x1024_1000f_8dpf_XYT.lineshifted.raw')
+        metadata = mtdt(filePath)
+        self.assertEqual(metadata.iniPath, self.iniPath)
+
     def testXmlPath(self):
         metadata = mtdt(self.rawPath)
+        self.assertEqual(metadata.xmlPath, self.xmlPath)
+
+    def testXmlPathLineShifted(self):
+        filePath = os.path.join(str(self.dataDir),
+                                '20190101_12_12_12_900nm_16x_512x1024_1000f_8dpf_XYT.lineshifted.raw')
+        metadata = mtdt(filePath)
         self.assertEqual(metadata.xmlPath, self.xmlPath)
 
     def testDate(self):
