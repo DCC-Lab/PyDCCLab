09e6450539989a251ed7e21751e992f08e1bc92a
MFournierQC <50327179+MFournierQC@users.noreply.github.com>
2019-07-19 10:42:27 -0400

    TestIniPath.

--- dcclab/tests/testRawMetadata.py
+++ dcclab/tests/testRawMetadata.py
@@ -6,9 +6,9 @@ import os
 
 class TestRawMetadata(env.DCCLabTestCase):
     def setUp(self) -> None:
-        self.rawPath = os.path.join(self.dataDir, '20190101_12_12_12_900nm_16x_512x1024_1000f_8dpf_XYT.raw')
-        self.iniPath = os.path.join(self.dataDir, '20190101_12_12_12_900nm_16x_512x1024_1000f_8dpf_XYT.ini')
-        self.xmlPath = os.path.join(self.dataDir, '20190101_12_12_12_900nm_16x_512x1024_1000f_8dpf_XYT.xml')
+        self.rawPath = os.path.join(str(self.dataDir), '20190101_12_12_12_900nm_16x_512x1024_1000f_8dpf_XYT.raw')
+        self.iniPath = os.path.join(str(self.dataDir), '20190101_12_12_12_900nm_16x_512x1024_1000f_8dpf_XYT.ini')
+        self.xmlPath = os.path.join(str(self.dataDir), '20190101_12_12_12_900nm_16x_512x1024_1000f_8dpf_XYT.xml')
 
     def tearDown(self) -> None:
         #os.remove(self.rawPath)
@@ -19,6 +19,10 @@ class TestRawMetadata(env.DCCLabTestCase):
     def testFileName(self):
         metadata = mtdt(self.rawPath)
         self.assertEqual(metadata.fileName, '20190101_12_12_12_900nm_16x_512x1024_1000f_8dpf_XYT')
+
+    def testIniPath(self):
+        metadata = mtdt(self.rawPath)
+        self.assertEqual(metadata.iniPath, self.iniPath)
 
 if __name__ == '__main__':
     unittest.main()
