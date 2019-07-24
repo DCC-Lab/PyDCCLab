8144162a5eb2778d75388e0a201e949f21477400
MFournierQC <50327179+MFournierQC@users.noreply.github.com>
2019-07-24 14:11:34 -0400

    Removed some obsolete comments.

--- pdkDatabase.py
+++ pdkDatabase.py
@@ -69,21 +69,5 @@ def createPDKDatabase():
 
 
 if __name__ == '__main__':
-    '''
-    # Reading the ini files.
-    directory = 'K:\\'
-    rawFiles = findFiles(directory, 'raw')
-    iniFiles = []
-
-    for rawFile in rawFiles:
-        iniFiles.append(re.sub('\.lineshifted\.raw|.raw', '.ini', rawFile, re.IGNORECASE))
-
-    for iniFile in iniFiles:
-        try:
-            with open(iniFile, 'r') as file:
-                print(file.readlines())
-        except:
-            print(">>>>{} file doesn't work.".format(iniFile))
-    '''
     createPDKDatabase()
     pass
