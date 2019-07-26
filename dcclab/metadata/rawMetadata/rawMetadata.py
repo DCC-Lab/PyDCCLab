ff7bd33c3b95951ab7881539233a1f22f590f155
MFournierQC <50327179+MFournierQC@users.noreply.github.com>
2019-07-26 09:18:17 -0400

    Correction to rawMetadata.

--- dcclab/metadata/rawMetadata/rawMetadata.py
+++ dcclab/metadata/rawMetadata/rawMetadata.py
@@ -5,6 +5,11 @@ import datetime
 
 
 class RAWMetadata:
+    # FixMe The PDK .raw files are weird to deal with. Their metadata is contained in others files, .ini and .xml,
+    #  that are in the same folder as the .raw file. However, we do not know yet if these files have valuable data.
+    #  Also, the .xml file seems rather poor in useful information. For now, the best thing to do is to deal with the
+    #  .xlsx files contained in PDK. The functions to read and extract metadata from .ini and .xml files are
+    #  placeholders, just in case.
     def __init__(self, rawPath):
         self.rawPath = rawPath
         # Data from file name.
@@ -13,11 +18,11 @@ class RAWMetadata:
 
         # Getting secondary files for metadata.
         self.iniPath = self.__iniPath()
-        self.xmlPath = self.__xmlPath()  # FixMe This might not be necessary. Delete if xml are irrelevant.
+        self.xmlPath = self.__xmlPath()
 
         # Data from secondary files.
         self.iniDict = self.extractDataFromIniFile()
-        self.xmlRoot = self.readXmlFile()  # FixMe This might not be necessary. Delete if xml are irrelevant.
+        self.xmlRoot = self.readXmlFile()
 
     def __fileName(self):
         file = os.path.basename(self.rawPath)
@@ -32,15 +37,14 @@ class RAWMetadata:
     def __iniPath(self):
         return re.sub('\.lineshifted\.raw|.raw', '.ini', self.rawPath, re.IGNORECASE)
 
-    def __xmlPath(self):  # FixMe This might not be necessary. Delete if xml are irrelevant.
+    def __xmlPath(self):
         return re.sub('XYT\.lineshifted\.raw|XYT.raw', 'OME.xml', self.rawPath, re.IGNORECASE)
 
-    def readIniFile(self):  # TODO What's important in the .ini file? Is there anything important?
+    def readIniFile(self):
         with open(self.iniPath, 'r') as file:
             lines = file.readlines()
         return lines
 
-    # TODO What about keys and types?
     def extractDataFromIniFile(self):
         keys = ['no.of.channels', 'frame.count', 'x.pixels', 'y.pixels', 'x.voltage', 'y.voltage', 'pixel.resolution',
                 'Laser.Power']
@@ -56,6 +60,9 @@ class RAWMetadata:
             except:
                 pass
         return iniDict
+
+    def getIniKeys(self):
+        pass
 
     def readXmlFile(self):
         tree = et.parse(self.xmlPath)
