62f6dad7c465fda3cd0649e8457e73a463be181b
MFournierQC <50327179+MFournierQC@users.noreply.github.com>
2019-08-01 10:57:19 -0400

    ValidateDataFile function meant for future expansion of file types depending on the research group.

--- dcclab/metadata/metadata.py
+++ dcclab/metadata/metadata.py
@@ -10,32 +10,64 @@ except:
 
 
 class Metadata:
+    # Supported research groups.
+    supportedResearchGroup = ['POM', 'PDK']
+
+    # Supported classes and formats for POM.
+    pomSupportedClasses = [CZIMetadata, CSVMetadata]
+    pomSupportedFormats = ['CZI', 'CSV']
+
+    # Supported classes and formats for PDK.
+    pdkSupportedClasses = [XLSXMetadata, RAWMetadata]
+    pdkSupportedFormats = ['XLSX', 'RAW']
+
     supportedClasses = [CZIMetadata, CSVMetadata, XLSXMetadata, RAWMetadata]
     supportedFormats = ['CZI', 'CSV', 'XLSX', 'RAW']
-    supportedResearchGroup = ['POM', 'PDK']
 
     def __init__(self, path: str):
         if path is not None:
             if not os.path.exists(path):
                 raise ValueError("Cannot load '{0}': file does not exist".format(path))
 
-            if not self.findResearchGroup(path):
-                raise ValueError("Cannot load '{}' : file is not from a supported research group.".format(path))
-
             self.path = path
-            self.__fileObject = None
+            self.__fileObject = self.validateDataFile()
+
+            '''
             for supportedClass in Metadata.supportedClasses:
                 try:
                     self.__fileObject = supportedClass(path)
                     break
                 except:
                     continue
+            '''
+
             if self.__fileObject is None:
                 message = "Cannot read '{0}': not a recognized format ({1})".format(self.path, Metadata.supportedFormats)
                 raise TypeError(message)
         else:
             self.path = None
             self.__fileObject = None
+
+    def validateDataFile(self):
+        researchGroup = self.findResearchGroup(self.path)
+        if researchGroup == 'POM':
+            for supportedClass in Metadata.pomSupportedClasses:
+                try:
+                    fileObject = supportedClass(self.path)
+                    return fileObject
+                except:
+                    continue
+            return None
+        elif researchGroup == 'PDK':
+            for supportedClass in Metadata.pdkSupportedClasses:
+                try:
+                    fileObject = supportedClass(self.path)
+                    return fileObject
+                except:
+                    continue
+            return None
+        else:
+            return None
 
     def findResearchGroup(self, path: str):
         basename = os.path.basename(path)
