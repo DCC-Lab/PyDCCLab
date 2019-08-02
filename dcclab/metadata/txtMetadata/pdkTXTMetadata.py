627f729b8bedfa5cc0c3341ec0373ed3d1510b34
MFournierQC <50327179+MFournierQC@users.noreply.github.com>
2019-08-02 11:20:30 -0400

    Added property tag to some functions in pdkTXTMetadata.

--- dcclab/metadata/txtMetadata/pdkTXTMetadata.py
+++ dcclab/metadata/txtMetadata/pdkTXTMetadata.py
@@ -1,14 +1,14 @@
 class PDKTXTMetadata:
     def __init__(self, path):
         self.path = path
-        self.dict = self.getDictFromFile()
 
     def readFile(self):
         with open(self.path, 'r') as file:
             lines = file.readlines()
         return lines
 
-    def getDictFromFile(self):
+    @property
+    def asDict(self):
         keys = ['no.of.channels', 'frame.count', 'x.pixels', 'y.pixels', 'x.voltage', 'y.voltage', 'pixel.resolution',
                 'Laser.Power']
         dictio = {}
@@ -24,6 +24,7 @@ class PDKTXTMetadata:
                 pass
         return dictio
 
-    def getKeys(self):
+    @property
+    def keys(self):
         return {'no.of.channels': 'INTEGER', 'frame.count': 'INTEGER', 'x.pixels': 'INTEGER', 'y.pixels': 'INTEGER',
                 'x.voltage': 'REAL', 'y.voltage': 'REAL', 'pixel.resolution': 'REAL', 'Laser.Power': 'REAL'}
