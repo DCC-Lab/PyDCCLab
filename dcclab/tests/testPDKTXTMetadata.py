bf15a6935b31c841184abb944073d5762d3629f0
MFournierQC <50327179+MFournierQC@users.noreply.github.com>
2019-08-02 14:47:32 -0400

    testPDKTXTMetadat first draft.

---
+++ dcclab/tests/testPDKTXTMetadata.py
@@ -0,0 +1,10 @@
+from dcclab import PDKTXTMetadata as mtdt
+import env
+
+
+class TestPDKTXTMetadata(env.DCCLabTestCase):
+    def setUp(self) -> None:
+        pass
+
+    def tearDown(self) -> None:
+        pass
