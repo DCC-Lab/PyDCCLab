c84586b906da3c43e89caee8c685b46a9e39af91
MFournierQC <50327179+MFournierQC@users.noreply.github.com>
2019-08-02 11:51:19 -0400

    xml for RAWMetadata is not implemented.

--- dcclab/metadata/rawMetadata/pdkRAWMetadata.py
+++ dcclab/metadata/rawMetadata/pdkRAWMetadata.py
@@ -33,9 +33,11 @@ class PDKRAWMetadata:
         return mtdt.asDict, mtdt.keys
 
     # FixMe Currently, we do not know if .xml files associated with .raw files have any valuable metadata.
+    @NotImplemented
     def __xmlPath(self):
         return re.sub('XYT\.lineshifted\.raw|XYT.raw', 'OME.xml', self.rawPath, re.IGNORECASE)
 
+    @NotImplemented
     def readXmlFile(self):
         tree = et.parse(self.xmlPath)
         return tree.getroot()
