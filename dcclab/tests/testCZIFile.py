a03d1032bac88eeac227a52e6db4f2147905b59c
Gabriel Genest <gabriel.genest.2@ulaval.ca>
2019-08-14 13:42:18 -0400

    Fixed some test
    Changed some czi files used for tests to use smaller files

--- dcclab/tests/testCZIFile.py
+++ dcclab/tests/testCZIFile.py
@@ -10,13 +10,13 @@ class TestConstructor(env.DCCLabTestCase):
 
     def testValidCziFile(self):
         try:
-            CZIFile(Path(self.dataDir / "testCziFileTwoChannels.czi"))
+            CZIFile(Path(self.dataDir / "testCziYX0Tiny.czi"))
         except InvalidFileFormatException:
             self.fail()
 
     def testInvalidNotCzi(self):
         with self.assertRaises(InvalidFileFormatException):
-            CZIFile(Path(self.dataDir / "testNotCziFile.jpg"))
+            CZIFile(Path(self.dataDir / "test.gif"))
 
     def testInvalidFileNotFound(self):
         with self.assertRaises(FileNotFoundError):
@@ -38,41 +38,23 @@ class TestConstructor(env.DCCLabTestCase):
 class TestMethodsAndProperties(env.DCCLabTestCase):
 
     def testNumberOfChannels(self):
-        czi3Channels = CZIFile(Path(self.dataDir / "testCziFileThreeChannelsFourTiles.czi"))
-        czi2Channels = CZIFile(Path(self.dataDir / "testCziFileTwoChannels.czi"))
-        cziOneChannel = CZIFile(Path(self.dataDir / "testCziOneChannel.czi"))
-        czi0Channel = CZIFile(Path(self.dataDir / "testCziFileYX0Axes.czi"))
-        self.assertEqual(czi3Channels.numberOfChannels, 3)
+        czi2Channels = CZIFile(Path(self.dataDir / "testCziFile.czi"))
+        cziOneChannel = CZIFile(Path(self.dataDir / "testOneChannel2Scenes.czi"))
+        czi0Channel = CZIFile(Path(self.dataDir / "testCziYX0Tiny.czi"))
         self.assertEqual(czi2Channels.numberOfChannels, 2)
         self.assertEqual(cziOneChannel.numberOfChannels, 1)
         self.assertEqual(czi0Channel.numberOfChannels, 0)
 
     def testTotalWidth(self):
-        czi1936x1460 = CZIFile(Path(self.dataDir / "testCziFileTwoChannels.czi"))
-        czi3790x2892 = CZIFile(Path(self.dataDir / "testCziFileThreeChannelsFourTiles.czi"))
-        czi954x670 = CZIFile(Path(self.dataDir / "testCziFileYX0Axes.czi"))
         czi2x3 = CZIFile(Path(self.dataDir / "testTinyCzi.czi"))
-        czi1525x905 = CZIFile(Path(self.dataDir / "testCziThreeChannelsOneScene.czi"))
         self.assertEqual(czi2x3.totalWidth, 2)
-        self.assertEqual(czi954x670.totalWidth, 954)
-        self.assertEqual(czi1525x905.totalWidth, 1525)
-        self.assertEqual(czi1936x1460.totalWidth, 1936)
-        self.assertEqual(czi3790x2892.totalWidth, 3790)
 
     def testTotalHeight(self):
-        czi1936x1460 = CZIFile(Path(self.dataDir / "testCziFileTwoChannels.czi"))
-        czi3790x2892 = CZIFile(Path(self.dataDir / "testCziFileThreeChannelsFourTiles.czi"))
-        czi954x670 = CZIFile(Path(self.dataDir / "testCziFileYX0Axes.czi"))
         czi2x3 = CZIFile(Path(self.dataDir / "testTinyCzi.czi"))
-        czi1525x905 = CZIFile(Path(self.dataDir / "testCziThreeChannelsOneScene.czi"))
         self.assertEqual(czi2x3.totalHeight, 3)
-        self.assertEqual(czi954x670.totalHeight, 670)
-        self.assertEqual(czi1525x905.totalHeight, 905)
-        self.assertEqual(czi1936x1460.totalHeight, 1460)
-        self.assertEqual(czi3790x2892.totalHeight, 2892)
 
     def testIsZStackNo(self):
-        czi = CZIFile(Path(self.dataDir / "testCziThreeChannelsOneScene.czi"))
+        czi = CZIFile(Path(self.dataDir / "testCziFileTwoScenesTiny.czi"))
         self.assertFalse(czi.isZstack)
 
     def testIsZStackYes(self):
@@ -80,7 +62,7 @@ class TestMethodsAndProperties(env.DCCLabTestCase):
         self.assertTrue(czi.isZstack)
 
     def testIsTimeSerieNo(self):
-        czi = CZIFile(Path(self.dataDir / "testCziThreeChannelsOneScene.czi"))
+        czi = CZIFile(Path(self.dataDir / "testCziFileTwoScenesTiny.czi"))
         self.assertFalse(czi.isTimeSerie)
 
     def testIsTimeSerieNoDim1(self):
@@ -88,60 +70,28 @@ class TestMethodsAndProperties(env.DCCLabTestCase):
         pass
 
     def testIsSceneNo(self):
-        czi = CZIFile(Path(self.dataDir / "testCziFileYX0Axes.czi"))
+        czi = CZIFile(Path(self.dataDir / "testCziYX0Tiny.czi"))
         self.assertFalse(czi.isScenes)
 
     def testIsSceneNoDim1(self):
-        czi = CZIFile(Path(self.dataDir / "testCziThreeChannelsOneScene.czi"))
+        czi = CZIFile(Path(self.dataDir / "testCziBSCZY0Tiny.czi"))
         self.assertFalse(czi.isScenes)
 
     def testIsScenesYes(self):
-        czi = CZIFile(Path(self.dataDir / "testCziMultipleScenes.czi"))
+        czi = CZIFile(Path(self.dataDir / "testCziFileTwoScenesTiny.czi"))
         self.assertTrue(czi.isScenes)
 
     def testShape(self):
-        czi1936x1460 = CZIFile(Path(self.dataDir / "testCziFileTwoChannels.czi"))
-        czi3790x2892 = CZIFile(Path(self.dataDir / "testCziFileThreeChannelsFourTiles.czi"))
-        czi954x670 = CZIFile(Path(self.dataDir / "testCziFileYX0Axes.czi"))
-        czi2x3 = CZIFile(Path(self.dataDir / "testTinyCzi.czi"))
-        czi1525x905 = CZIFile(Path(self.dataDir / "testCziThreeChannelsOneScene.czi"))
-        self.assertTupleEqual(czi2x3.shape, (1, 2, 3, 2, 1))
-        self.assertTupleEqual(czi954x670.shape, (670, 954, 3))
-        self.assertTupleEqual(czi1525x905.shape, (1, 1, 3, 905, 1525, 1))
+        czi1936x1460 = CZIFile(Path(self.dataDir / "testCziFile.czi"))
+        czi2x3 = CZIFile(Path(self.dataDir / "testCziZStack4Tiny.czi"))
+        czi2x2 = CZIFile(Path(self.dataDir / "testCziYX0Tiny.czi"))
+        czi2x3_2 = CZIFile(Path(self.dataDir / "testTinyCzi.czi"))
+        self.assertTupleEqual(czi2x3_2.shape, (1, 2, 3, 2, 1))
+        self.assertTupleEqual(czi2x2.shape, (2, 2, 3))
         self.assertTupleEqual(czi1936x1460.shape, (1, 2, 1460, 1936, 1))
-        self.assertTupleEqual(czi3790x2892.shape, (1, 1, 3, 2892, 3790, 1))
+        self.assertTupleEqual(czi2x3.shape, (1, 2, 4, 3, 2, 1))
 
-    def testTileMapKeysOnlyTiles(self):
-        czi3Channels = CZIFile(Path(self.dataDir / "testCziFileThreeChannelsFourTiles.czi"))
-        keys = list(czi3Channels.tileMap.keys())
-        supposedKeys = [(range(60, 1996), range(0, 1460), None, None, None, 0),
-                        (range(60, 1996), range(0, 1460), None, None, None, 1),
-                        (range(60, 1996), range(0, 1460), None, None, None, 2),
-                        (range(1854, 3790), range(80, 1540), None, None, None, 0),
-                        (range(1854, 3790), range(80, 1540), None, None, None, 1),
-                        (range(1854, 3790), range(80, 1540), None, None, None, 2),
-                        (range(1794, 3730), range(1432, 2892), None, None, None, 0),
-                        (range(1794, 3730), range(1432, 2892), None, None, None, 1),
-                        (range(1794, 3730), range(1432, 2892), None, None, None, 2),
-                        (range(0, 1936), range(1352, 2812), None, None, None, 0),
-                        (range(0, 1936), range(1352, 2812), None, None, None, 1),
-                        (range(0, 1936), range(1352, 2812), None, None, None, 2)]
-        self.assertListEqual(keys, supposedKeys)
 
-    def testTileMapKeysOnlyOneTile(self):
-        czi = CZIFile(Path(self.dataDir / "testCziOneChannel.czi"))
-        keys = list(czi.tileMap.keys())
-        supposedKeys = [(range(0, 857), range(0, 610), None, None, None, 0)]
-        self.assertListEqual(keys, supposedKeys)
-
-    def testTileMapKeysMultipleScenes(self):
-        czi = CZIFile(Path(self.dataDir / "testCziMultipleScenes.czi"))
-        keys = list(czi.tileMap.keys())
-        supposedKeys = [(range(0, 447), range(0, 357), None, 1, None, 0),
-                        (range(0, 447), range(0, 357), None, 1, None, 1),
-                        (range(0, 447), range(0, 357), None, 0, None, 0),
-                        (range(0, 447), range(0, 357), None, 0, None, 1)]
-        self.assertListEqual(keys, supposedKeys)
 
     def testTileMapKeysZStack(self):
         czi = CZIFile(Path(self.dataDir / "testCziZStack4.czi"))
@@ -153,11 +103,11 @@ class TestMethodsAndProperties(env.DCCLabTestCase):
         self.assertListEqual(keys, supposedKeys)
 
     def testTileMapYX0None(self):
-        czi = CZIFile(Path(self.dataDir / "testCziFileYX0Axes.czi"))
+        czi = CZIFile(Path(self.dataDir / "testCziYX0Tiny.czi"))
         self.assertIsNone(czi.tileMap)
 
     def testAxesYX0(self):
-        czi = CZIFile(Path(self.dataDir / "testCziFileYX0Axes.czi"))
+        czi = CZIFile(Path(self.dataDir / "testCziYX0Tiny.czi"))
         self.assertEqual(czi.axes, "YX0")
 
     def testAxesBCZYX0(self):
@@ -165,15 +115,15 @@ class TestMethodsAndProperties(env.DCCLabTestCase):
         self.assertEqual(czi.axes, "BCZYX0")
 
     def testAxesBCYX0(self):
-        czi = CZIFile(Path(self.dataDir / "testCziFileTwoChannels.czi"))
+        czi = CZIFile(Path(self.dataDir / "testCziFile.czi"))
         self.assertEqual(czi.axes, "BCYX0")
 
     def testAxesBSCYX0(self):
-        czi = CZIFile(Path(self.dataDir / "testCziThreeChannelsOneScene.czi"))
+        czi = CZIFile(Path(self.dataDir / "testOneChannel2Scenes.czi"))
         self.assertEqual(czi.axes, "BSCYX0")
 
     def testOriginalDType(self):
-        czi = CZIFile(Path(self.dataDir / "testCziThreeChannelsOneScene.czi"))
+        czi = CZIFile(Path(self.dataDir / "testCziFile.czi"))
         self.assertEqual(czi.originalDType, np.uint16)
 
     def testAllDataYX0(self):
@@ -181,17 +131,18 @@ class TestMethodsAndProperties(env.DCCLabTestCase):
         allData = czi.allData()
         data = np.dstack((np.array([[7, 7], [6, 6]]), np.array([[46, 48], [51, 47]]), np.array([[32, 29], [33, 28]])))
         # FIXME: should try to get version of package.
-        self.assertTrue(np.array_equal(allData, data),"It is possible cziFile is an old version.  pip install czifile --upgrade")
-        
+        self.assertTrue(np.array_equal(allData, data),
+                        "It is possible cziFile is an old version.  pip install czifile --upgrade")
+
     def testImageDataYX0Axes(self):
         try:
-            czi = CZIFile(Path(self.dataDir / "testCziFileYX0Axes.czi"))
+            czi = CZIFile(Path(self.dataDir / "testCziYX0Tiny.czi"))
             self.assertIsNotNone(czi.imageData())
         except:
             self.fail("Exception raised.")
 
     def testImageDataMultipleImagesException(self):
-        czi = CZIFile(Path(self.dataDir / "testCziMultipleScenes.czi"))
+        czi = CZIFile(Path(self.dataDir / "testCziFileTwoScenesTiny.czi"))
         with self.assertRaises(ValueError):
             czi.imageData()
 
@@ -199,7 +150,8 @@ class TestMethodsAndProperties(env.DCCLabTestCase):
         czi = CZIFile(Path(self.dataDir / "testCziYX0Tiny.czi"))
         values = np.dstack((np.array([[7, 7], [6, 6]]), np.array([[46, 48], [51, 47]]), np.array([[32, 29], [33, 28]])))
         # FIXME: should try to get version of package.
-        self.assertTrue(np.array_equal(czi.imageData().asArray(), values.transpose((1, 0, 2))),"It is possible cziFile is an old version.  pip install czifile --upgrade")
+        self.assertTrue(np.array_equal(czi.imageData().asArray(), values.transpose((1, 0, 2))),
+                        "It is possible cziFile is an old version.  pip install czifile --upgrade")
 
     def testImageDataBSCYX0Values(self):
         czi = CZIFile(Path(self.dataDir / "testCziBSCZY0Tiny.czi"))
@@ -207,12 +159,8 @@ class TestMethodsAndProperties(env.DCCLabTestCase):
                             np.array([[510, 511], [505, 497]])))
         self.assertTrue(np.array_equal(czi.imageData().asArray(), values.transpose((1, 0, 2))))
 
-    def testImageData2DArrayTo3DArray(self):
-        czi = CZIFile(Path(self.dataDir / "testCziOneChannel.czi"))
-        self.assertTupleEqual(czi.imageData().shape, (857, 610, 1))
-
     def testScenesDataOneSceneNone(self):
-        czi = CZIFile(Path(self.dataDir / "testCziThreeChannelsOneScene.czi"))
+        czi = CZIFile(Path(self.dataDir / "testCziBSCZY0Tiny.czi"))
         self.assertIsNone(czi.scenesData())
 
     def testScenesDataValues(self):
