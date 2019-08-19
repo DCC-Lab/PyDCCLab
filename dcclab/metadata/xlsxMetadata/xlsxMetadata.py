e66bd7f5049ad809eeda480894d4093944d736b4
MFournierQC <50327179+MFournierQC@users.noreply.github.com>
2019-08-19 13:20:27 -0400

    Reworked XLSXMetadata as a base class.

--- dcclab/metadata/xlsxMetadata/xlsxMetadata.py
+++ dcclab/metadata/xlsxMetadata/xlsxMetadata.py
@@ -1,6 +1,5 @@
 import xlrd
 import os
-import re
 
 
 class XLSXMetadata:
@@ -10,7 +9,6 @@ class XLSXMetadata:
 
         self.workbook = self.getWorkbook()
         self.worksheets = self.getWorksheets()
-        self.keys = self.getKeys()
 
     def fileName(self):
         file = os.path.basename(self.path)
@@ -31,39 +29,10 @@ class XLSXMetadata:
         except:
             return worksheets
 
-    def getKeys(self) -> dict:
-        keys = {}
-        try:
-            for worksheet in self.worksheets:
-                header = {}
-                for col in range(worksheet.ncols):
-                    key = self.formatKey(str(worksheet.cell_value(0, col)))
-                    if key == 'Folder_path':
-                        header[key] = 'TEXT PRIMARY KEY'
-                    else:
-                        header[key] = 'TEXT'
-                keys[worksheet.name] = header
-            return keys
-        except:
-            return keys
+    @property
+    def asDict(self):
+        return {}
 
     @property
-    def asDict(self) -> dict:
-        dictio = {}
-        for worksheet in self.worksheets:
-            sheet = {}
-            for row in range(1, worksheet.nrows):
-                cols = {}
-                for col in range(worksheet.ncols):
-                    key = self.formatKey(str(worksheet.cell_value(0, col)))
-                    cols[key] = str(worksheet.cell_value(row, col)).replace(',', '')
-                sheet[row] = cols
-            dictio[worksheet.name] = sheet
-        return dictio
-
-    def formatKey(self, key: str):
-        formattedKey = key.replace('(Hz)', '')
-        formattedKey = re.sub('^\\s{1,99}', '', formattedKey)
-        formattedKey = re.sub('\\s{1,99}$', '', formattedKey)
-        formattedKey = re.sub('\\s', '_', formattedKey)
-        return formattedKey
+    def keys(self):
+        return {}
