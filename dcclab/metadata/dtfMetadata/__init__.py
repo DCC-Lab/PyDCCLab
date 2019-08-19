2210f8a7ec65f38a78e697ddabd732ea497c1348
MFournierQC <50327179+MFournierQC@users.noreply.github.com>
2019-08-19 10:56:54 -0400

    Corrected a small mistake in naming convention.

--- dcclab/metadata/dtfMetadata/__init__.py
+++ dcclab/metadata/dtfMetadata/__init__.py
@@ -1,4 +1,4 @@
 """ We import almost everything by default, in the general
 namespace because it is simpler for everyone """
 
-from .DTFMetadata import DTFMetadata
+from .dtfMetadata import DTFMetadata
