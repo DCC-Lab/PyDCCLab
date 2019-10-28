eb9eeb64f4ff6be55298ccb0fcb411f8ac135a7f
Gabriel Genest <gabriel.genest.2@ulaval.ca>
2019-10-28 18:52:15 -0400

    Reworked the correlation matrix

--- dcclab/correlationMatrix.py
+++ dcclab/correlationMatrix.py
@@ -3,6 +3,8 @@ import pandas as pd
 from .DCCExceptions import *
 import seaborn as sns
 import matplotlib.pyplot as plt
+import typing as tp
+from datetime import datetime
 
 
 class CSVReader:
@@ -17,35 +19,52 @@ class CSVReader:
                 "Problem occurred when reading '{}'. Please make sure it is a valid csv file. Exception message:\n{}".format(
                     filename, e))
 
-    def dataframeAsArray(self, dataframeDropFirstColumn: bool = True, castValuesTo=float):
-        df: pd.DataFrame = self.dataframe.copy()
-        firstColumn = None
-        if dataframeDropFirstColumn:
-            firstColumn = np.array(df[df.columns[0]])
-            df.drop(df.columns[0], axis=1, inplace=True)
-        df = df.astype(castValuesTo)
-        return (np.array(df), firstColumn) if dataframeDropFirstColumn else np.array(df)
-
-    def dropColumns(self, columnsToDrop: tuple = None):
-        if columnsToDrop is not None:
-            for column in columnsToDrop:
-                self.dataframe.drop(self.dataframe.columns[column], axis=1, inplace=True)
-
 
 class CorrelationMatrix:
 
-    def __init__(self, array: np.ndarray):
-        if not isinstance(array, np.ndarray):
-            raise TypeError("Array parameter must be a numpy.ndarray instance.")
-        self.array = array
+    def __init__(self, dataframe: pd.DataFrame = None, array: np.ndarray = None,
+                 headers: tp.Union[list, np.ndarray] = None):
+        if dataframe is None and array in None:
+            raise ValueError("A dataframe or an array is required to compute a correlation matrix.")
+        if dataframe is not None and array is not None:
+            raise ValueError(
+                "A dataframe and an array were given. Please choose one of the two objects to compute the correlation matrix")
+        if array is not None:
+            self.dataframe = pd.DataFrame(array, columns=headers)
+        else:
+            self.dataframe = dataframe
 
-    def computeCorrelationMatrix(self, rowsAreParameters: bool = False):
-        return np.corrcoef(self.array, rowvar=rowsAreParameters)
+    def computeCorrelationMatrix(self, castTo: type = float):
+        corr = self.dataframe.astype(castTo).corr()
+        return corr
 
-    def displayCorrelationMatrix(self, cmap: str = None, annotations: bool = True, colorbar: bool = True,
-                                 tickLabel: list = "auto"):
-        correlationMatrix = self.computeCorrelationMatrix()
-        heatmap = sns.heatmap(correlationMatrix, vmin=-1, vmax=1, cmap=cmap, annot=annotations, cbar=colorbar,
-                              xticklabels=tickLabel, yticklabels=tickLabel)
+    def showCorrelationMatrix(self, castTo: type = float):
+        corr = self.computeCorrelationMatrix(castTo)
+        ax = sns.heatmap(
+            corr,
+            vmin=-1, vmax=1, center=0,
+            cmap=sns.diverging_palette(20, 220, n=200),
+            square=True
+        )
+        ax.set_xticklabels(
+            ax.get_xticklabels(),
+            rotation=45,
+            horizontalalignment='right'
+        )
         plt.show()
-        return correlationMatrix
+
+
+class DataframeUtils:
+
+    @staticmethod
+    def ageOfMouse(dataframeRow, format: str = "%Y-%m-%d"):
+        age = np.nan
+        dateOfBirth = dataframeRow["DDN"]
+        dateOfDeath = dataframeRow["date_mort"]
+        print(dateOfBirth)
+        if dateOfBirth is not None and dateOfDeath is not None:
+            dateOfBirth = datetime.strptime(dateOfBirth, format)
+            dateOfDeath = datetime.strptime(dateOfDeath, format)
+            age = dateOfBirth - dateOfDeath
+            age = age.days
+        return age
