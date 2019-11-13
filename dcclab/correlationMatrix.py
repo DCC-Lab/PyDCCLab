e92218e7f118e4f933901231c7a0a5bf4de2e028
Gabriel Genest <gabriel.genest.2@ulaval.ca>
2019-11-13 14:10:20 -0500

    Changed a lot of things
    and added new stuff

--- dcclab/correlationMatrix.py
+++ dcclab/correlationMatrix.py
@@ -24,47 +24,102 @@ class CorrelationMatrix:
 
     def __init__(self, dataframe: pd.DataFrame = None, array: np.ndarray = None,
                  headers: tp.Union[list, np.ndarray] = None):
-        if dataframe is None and array in None:
+        if dataframe is None and array is None:
             raise ValueError("A dataframe or an array is required to compute a correlation matrix.")
         if dataframe is not None and array is not None:
             raise ValueError(
                 "A dataframe and an array were given. Please choose one of the two objects to compute the correlation matrix")
+        if dataframe is not None and not isinstance(dataframe, pd.DataFrame):
+            raise TypeError("Dataframe parameter must be a pandas.Dataframe instance.")
         if array is not None:
             self.dataframe = pd.DataFrame(array, columns=headers)
         else:
             self.dataframe = dataframe
 
-    def computeCorrelationMatrix(self, castTo: type = float):
+    def computeCorrelationMatrix(self, castTo: type = float, columnsToDrop: list = None):
+        if columnsToDrop is not None:
+            if all(isinstance(x, int) for x in columnsToDrop):
+                self.dataframe.drop(self.dataframe.columns[columnsToDrop], axis=1, inplace=True)
+            elif all(isinstance(x, str) for x in columnsToDrop):
+                self.dataframe.drop(columnsToDrop, axis=1, inplace=True)
+            else:
+                raise ValueError("The list of columns to drop must consist of integers only or strings only.")
         corr = self.dataframe.astype(castTo).corr()
         return corr
 
-    def showCorrelationMatrix(self, castTo: type = float):
+    def showCorrelationMatrix(self, castTo: type = float, annotation: bool = True, fontScale: float = 0.5,
+                              title: str = None, titleFontSize: int = 16):
         corr = self.computeCorrelationMatrix(castTo)
+        sns.set(font_scale=fontScale)
         ax = sns.heatmap(
             corr,
             vmin=-1, vmax=1, center=0,
             cmap=sns.diverging_palette(20, 220, n=200),
-            square=True
+            square=True,
+            annot=annotation
         )
         ax.set_xticklabels(
             ax.get_xticklabels(),
             rotation=45,
             horizontalalignment='right'
         )
+        plt.title(title, fontsize=titleFontSize)
         plt.show()
 
 
 class DataframeUtils:
 
     @staticmethod
-    def ageOfMouse(dataframeRow, format: str = "%Y-%m-%d"):
+    def ageOfMouse(dataframe: pd.DataFrame, format: str = "%Y-%m-%d"):
         age = np.nan
-        dateOfBirth = dataframeRow["DDN"]
-        dateOfDeath = dataframeRow["date_mort"]
-        print(dateOfBirth)
-        if dateOfBirth is not None and dateOfDeath is not None:
+        dateOfBirth = dataframe["DDN"]
+        dateOfDeath = dataframe["date_mort"]
+        if dateOfBirth != " " and dateOfDeath != " ":
             dateOfBirth = datetime.strptime(dateOfBirth, format)
             dateOfDeath = datetime.strptime(dateOfDeath, format)
-            age = dateOfBirth - dateOfDeath
+            age = dateOfDeath - dateOfBirth
             age = age.days
+            if age < 0:
+                age = np.nan
         return age
+
+    @staticmethod
+    def nbDaysBetweenDeathAndUse(dataframe: pd.DataFrame, format: str = "%Y-%m-%d"):
+        days = np.nan
+        dateUse = dataframe["date_utilisation"]
+        dateOfDeath = dataframe["date_mort"]
+        if dateOfDeath != " " and dateUse != " ":
+            dateUse = datetime.strptime(dateUse, format)
+            dateOfDeath = datetime.strptime(dateOfDeath, format)
+            days = dateUse - dateOfDeath
+            days = days.days
+        if days < 0:
+            days = np.nan
+        return days
+
+    @staticmethod
+    def injectionVolume(dataframe: pd.DataFrame):
+        volumeInjection = dataframe["volume_injection"]
+        if volumeInjection == "2 x 200 nl (G+D)":
+            injVol = 400
+        else:
+            try:
+                injVol = float(volumeInjection)
+            except ValueError:
+                injVol = np.nan
+        return injVol
+
+    @staticmethod
+    def usedValuesStatsPerColumn(dataframe: pd.DataFrame):
+        d = dict()
+        for column in dataframe.columns:
+            array = np.array(dataframe[column])
+            total = array.size
+            nonNan = np.count_nonzero(~np.isnan(array))
+            d[column] = (nonNan / total, nonNan, total)
+        return d
+
+    @staticmethod
+    def dropRowsWithCertainValues(dataframe: pd.DataFrame, value, columnName: str) -> pd.DataFrame:
+        newDataframe = dataframe[dataframe[columnName] != value]
+        return newDataframe
--- dcclab/tests/correlationMatrixTest.py
+++ dcclab/tests/correlationMatrixTest.py
@@ -30,61 +30,38 @@ class TestCSVReader(env.DCCLabTestCase):
     def testDataframeAsArrayNoDrop(self):
         comaSep = Path(self.dataDir / "testCSVReader.csv")
         csv = CSVReader(comaSep)
-        arrayFromMethod = csv.dataframeAsArray(False)
+        dataframe = csv.dataframe
         array = np.array([[1, 10.2, 12.3, 89], [2, 3652.3, 74.25, 568], [3, 452.2, 4125, 1], [4, 89.25, 687.1254, 0.23],
                           [5, 784.2, 71.2, 12], [6, 12.25, 123.456789, 89], [7, 412.2, 321, 56], [8, 36, 36, 36],
                           [9, 78, 78, 7414]])
-        # Problem with float and ==
-        self.assertTrue(np.allclose(array, arrayFromMethod))
+        self.assertTrue(np.allclose(array, dataframe))
 
     def testDataframeAsArrayDropFirstColumn(self):
         comaSep = Path(self.dataDir / "testCSVReader.csv")
         csv = CSVReader(comaSep)
-        arrayFromMethod = csv.dataframeAsArray(True)
-        array = np.array([[10.2, 12.3, 89], [3652.3, 74.25, 568], [452.2, 4125, 1], [89.25, 687.1254, 0.23],
-                          [784.2, 71.2, 12], [12.25, 123.456789, 89], [412.2, 321, 56], [36, 36, 36],
-                          [78, 78, 7414]])
-        self.assertTrue(np.allclose(array, arrayFromMethod[0]))
-        self.assertTrue(np.array_equal(arrayFromMethod[1], [1, 2, 3, 4, 5, 6, 7, 8, 9]))
+        dataframe = csv.dataframe
+        array = np.array([[1, 10.2, 12.3, 89], [2, 3652.3, 74.25, 568], [3, 452.2, 4125, 1], [4, 89.25, 687.1254, 0.23],
+                          [5, 784.2, 71.2, 12], [6, 12.25, 123.456789, 89], [7, 412.2, 321, 56], [8, 36, 36, 36],
+                          [9, 78, 78, 7414]])
+        self.assertTrue(np.allclose(dataframe, array))
 
     def testDataframeAsArraySameDataframe(self):
         comaSep = Path(self.dataDir / "testCSVReader.csv")
         otherSep = Path(self.dataDir / "testCSVReaderOtherSep.csv")
         csvReader1 = CSVReader(comaSep)
         csvReader2 = CSVReader(otherSep, ";")
-        array1Drop = csvReader1.dataframeAsArray()
-        array1NoDrop = csvReader1.dataframeAsArray(False)
-        array2Drop = csvReader2.dataframeAsArray()
-        array2NoDrop = csvReader2.dataframeAsArray(False)
-        self.assertTrue(np.array_equal(array1Drop[1], array2Drop[1]))
-        self.assertTrue(np.array_equal(array1Drop[0], array2Drop[0]))
-        self.assertTrue(np.array_equal(array1NoDrop, array2NoDrop))
+        dataframe1 = csvReader1.dataframe
+        dataframe2 = csvReader2.dataframe
+        self.assertTrue(np.array_equal(dataframe1, dataframe2))
 
     def testDataFrameAsArrayInt(self):
         comaSep = Path(self.dataDir / "testCSVReader.csv")
         csvReader1 = CSVReader(comaSep)
-        arrayInt = csvReader1.dataframeAsArray(False, int)
+        arrayInt = np.array(csvReader1.dataframe, dtype=int)
         array = np.array([[1, 10.2, 12.3, 89], [2, 3652.3, 74.25, 568], [3, 452.2, 4125, 1], [4, 89.25, 687.1254, 0.23],
                           [5, 784.2, 71.2, 12], [6, 12.25, 123.456789, 89], [7, 412.2, 321, 56], [8, 36, 36, 36],
                           [9, 78, 78, 7414]], dtype=int)
         self.assertTrue(np.array_equal(arrayInt, array))
-
-    def testDataframeAsArrayTwoDifferentCastType(self):
-        comaSep = Path(self.dataDir / "testCSVReader.csv")
-        csvReader1 = CSVReader(comaSep)
-        arrayInt = csvReader1.dataframeAsArray(False, int)
-        arrayFloat = csvReader1.dataframeAsArray(False)
-        self.assertFalse(np.array_equal(arrayInt, arrayFloat))
-
-    def testDropColumns(self):
-        comaSep = Path(self.dataDir / "testCSVReader.csv")
-        comaSep2 = Path(self.dataDir / "testCSVReader.csv")
-        csvReader1 = CSVReader(comaSep)
-        dataframeNoTouch = CSVReader(comaSep2)
-        csvReader1.dropColumns((1, 2))
-        originalArray = dataframeNoTouch.dataframeAsArray(False)
-        removedColumns = csvReader1.dataframeAsArray(False)
-        self.assertFalse(np.array_equal(originalArray, removedColumns))
 
 
 class TestCorrelationMatrix(env.DCCLabTestCase):
@@ -93,33 +70,213 @@ class TestCorrelationMatrix(env.DCCLabTestCase):
         with self.assertRaises(TypeError):
             CorrelationMatrix("1234556776543wdfghytf")
 
-    def testConstructorValid(self):
+    def testConstructorInvalidAllNone(self):
+        with self.assertRaises(ValueError):
+            CorrelationMatrix()
+
+    def testConstructorInvalidBothGiven(self):
+        array = np.random.randint(0, 1253, (4, 10))
+        dataframe = pd.DataFrame(array)
+        with self.assertRaises(ValueError):
+            CorrelationMatrix(dataframe, array)
+
+    def testConstructorValidArray(self):
         array = np.array([[1, 2, 3, 4, 5, 6], [6, 5, 4, 3, 2, 1]])
         try:
-            CorrelationMatrix(array)
+            CorrelationMatrix(array=array)
         except:
             self.fail("No exception should be thrown.")
+
+    def testConstructorValidDataframe(self):
+        comaSep = Path(self.dataDir / "testCSVReader.csv")
+        dataframe = CSVReader(comaSep).dataframe
+        try:
+            CorrelationMatrix(dataframe)
+        except:
+            self.fail("No exception should be thrown.")
+
+    def testConstructorReturnsDataframeWithArrayGivenNoHeader(self):
+        array = np.array([[1, 2, 3, 4, 5, 6], [6, 5, 4, 3, 2, 1]])
+        cm = CorrelationMatrix(array=array)
+        dataframe = cm.dataframe
+        self.assertEqual("   0  1  2  3  4  5\n0  1  2  3  4  5  6\n1  6  5  4  3  2  1", str(dataframe))
+
+    def testConstructorReturnsDataframeWithArrayGiven(self):
+        array = np.array([[1, 2, 3, 4, 5, 6], [6, 5, 4, 3, 2, 1]])
+        headers = ["A", "B", "C", "D", "E", "F"]
+        cm = CorrelationMatrix(array=array, headers=headers)
+        dataframe = cm.dataframe
+        self.assertEqual("   A  B  C  D  E  F\n0  1  2  3  4  5  6\n1  6  5  4  3  2  1", str(dataframe))
 
     def testCorrelationCompute(self):
         comaSep = Path(self.dataDir / "testCSVReader.csv")
         csvReader1 = CSVReader(comaSep)
-        array, firstColumn = csvReader1.dataframeAsArray()
-        corr = CorrelationMatrix(array)
-        correlationMatrix = corr.computeCorrelationMatrix()
-        self.assertIsInstance(correlationMatrix, np.ndarray)
+        dataframe = csvReader1.dataframe
+        corr = CorrelationMatrix(dataframe)
+        correlationMatrix = corr.computeCorrelationMatrix(columnsToDrop=[0])
+        self.assertIsInstance(correlationMatrix, pd.DataFrame)
 
-    def testCorrelationComputeRowsAreParameters(self):
-        array = np.array([[235, 236, 245.25], [23.25, 19.58, 47.36], [-12.25, -12, -13.0005]])
-        corr = CorrelationMatrix(array)
-        correlationMatrix = corr.computeCorrelationMatrix(True)
-        print(correlationMatrix)
-        self.assertIsInstance(correlationMatrix, np.ndarray)
+    def testCorrelationComputeWithDrop(self):
+        array = np.array([[1, 2, 3, 4, 5, 6], [6, 5, 4, 3, 2, 1]])
+        headers = ["A", "B", "C", "D", "E", "F"]
+        cm = CorrelationMatrix(array=array, headers=headers)
+        corrMatrixNoDrop = cm.computeCorrelationMatrix()
+        corrMatrix = cm.computeCorrelationMatrix(columnsToDrop=["A", "B"])
+        self.assertNotEqual(corrMatrix.shape, corrMatrixNoDrop.shape)
 
-    def testCorrelationMatrixValues(self):
-        array = np.array([[10, 13, 1], [11, 14, 1], [10, 15, 0]])
-        corr = CorrelationMatrix(array)
-        correlationMatrix = corr.computeCorrelationMatrix()
-        print(correlationMatrix)
+    def testCorrelationComputeWithDropIndices(self):
+        array = np.array([[1, 2, 3, 4, 5, 6], [6, 5, 4, 3, 2, 1]])
+        headers = ["A", "B", "C", "D", "E", "F"]
+        cm = CorrelationMatrix(array=array, headers=headers)
+        corrMatrixNoDrop = cm.computeCorrelationMatrix()
+        corrMatrix = cm.computeCorrelationMatrix(columnsToDrop=[0, 1])
+        self.assertNotEqual(corrMatrix.shape, corrMatrixNoDrop.shape)
+
+    def testCorrelationComputeDropBothWays(self):
+        array = np.array([[1, 2, 3, 4, 5, 6], [6, 5, 4, 3, 2, 1]])
+        headers = ["A", "B", "C", "D", "E", "F"]
+        cm = CorrelationMatrix(array=array, headers=headers)
+        cm2 = CorrelationMatrix(array=array, headers=headers)
+        correlationMatrixDropIndices = cm.computeCorrelationMatrix(columnsToDrop=[1, 2, 5])
+        correlationMatrixDropNames = cm2.computeCorrelationMatrix(columnsToDrop=["B", "C", "F"])
+        self.assertTrue(np.array_equal(correlationMatrixDropIndices, correlationMatrixDropNames))
+
+    def testCorrelationComputeDropNotIntNorStrings(self):
+        array = np.array([[1, 2, 3, 4, 5, 6], [6, 5, 4, 3, 2, 1]])
+        headers = ["A", "B", "C", "D", "E", "F"]
+        cm = CorrelationMatrix(array=array, headers=headers)
+        cm2 = CorrelationMatrix(array=array, headers=headers)
+        with self.assertRaises(ValueError):
+            cm.computeCorrelationMatrix(columnsToDrop=[4j + 3, 23, "hello world"])
+        with self.assertRaises(ValueError):
+            cm2.computeCorrelationMatrix(columnsToDrop=["A", "B", 2])
+
+    @patch("matplotlib.pyplot.show", new=Mock())
+    def testCorrelationMatrixShow(self):
+        array = np.array([[1, 2, 3, 4, 5, 6], [6, 5, 4, 3, 2, 1]])
+        headers = ["A", "B", "C", "D", "E", "F"]
+        cm = CorrelationMatrix(array=array, headers=headers)
+        try:
+            cm.showCorrelationMatrix()
+        except:
+            self.fail("Exception raised.")
+
+
+class TestDataframeUtils(env.DCCLabTestCase):
+
+    def testAgeOfMouseNotNegative(self):
+        birthDate = "2018-05-09"
+        deathDate = "2019-08-02"
+        birthDate2 = "2018-05-10"
+        deathDate2 = "2019-08-02"
+        birthDate3 = "2018-05-11"
+        deathDate3 = "2019-08-02"
+        array = np.array([[birthDate, deathDate], [birthDate2, deathDate2], [birthDate3, deathDate3]], dtype=str)
+        headers = ["DDN", "date_mort"]
+        dataframe = pd.DataFrame(array, columns=headers)
+        dataframe["age"] = dataframe.apply(lambda row: DataframeUtils.ageOfMouse(row), axis=1)
+        newArray = [[birthDate, deathDate, 450], [birthDate2, deathDate2, 449], [birthDate3, deathDate3, 448]]
+        newHeaders = ["DDN", "date_mort", "age"]
+        finalDataframe = pd.DataFrame(newArray, columns=newHeaders)
+        self.assertTrue(finalDataframe.equals(dataframe))
+
+    def testAgeOfMouseNegative(self):
+        deathDate = "2018-05-09"
+        birthDate = "2019-08-02"
+        deathDate2 = "2018-05-10"
+        birthDate2 = "2019-08-02"
+        deathDate3 = "2018-05-11"
+        birthDate3 = "2019-08-02"
+        array = np.array([[birthDate, deathDate], [birthDate2, deathDate2], [birthDate3, deathDate3]], dtype=str)
+        headers = ["DDN", "date_mort"]
+        dataframe = pd.DataFrame(array, columns=headers)
+        dataframe["age"] = dataframe.apply(lambda row: DataframeUtils.ageOfMouse(row), axis=1)
+        newArray = [[birthDate, deathDate, np.nan], [birthDate2, deathDate2, np.nan], [birthDate3, deathDate3, np.nan]]
+        newHeaders = ["DDN", "date_mort", "age"]
+        finalDataframe = pd.DataFrame(newArray, columns=newHeaders)
+        self.assertTrue(finalDataframe.equals(dataframe))
+
+    def testDeathUseDelayNotNegative(self):
+        deathDate = "2018-05-09"
+        useDate = "2019-08-02"
+        deathDate2 = "2018-05-10"
+        useDate2 = "2019-08-02"
+        deathDate3 = "2018-05-11"
+        useDate3 = "2019-08-02"
+        array = np.array([[deathDate, useDate], [deathDate2, useDate2], [deathDate3, useDate3]], dtype=str)
+        headers = ["date_mort", "date_utilisation"]
+        dataframe = pd.DataFrame(array, columns=headers)
+        dataframe["delay"] = dataframe.apply(lambda row: DataframeUtils.nbDaysBetweenDeathAndUse(row), axis=1)
+        newArray = [[deathDate, useDate, 450], [deathDate2, useDate2, 449], [deathDate3, useDate3, 448]]
+        newHeaders = ["date_mort", "date_utilisation", "delay"]
+        finalDataframe = pd.DataFrame(newArray, columns=newHeaders)
+        self.assertTrue(finalDataframe.equals(dataframe))
+
+    def testDeathUseDelayNegative(self):
+        useDate = "2018-05-09"
+        deathDate = "2019-08-02"
+        useDate2 = "2018-05-10"
+        deathDate2 = "2019-08-02"
+        useDate3 = "2018-05-11"
+        deathDate3 = "2019-08-02"
+        array = np.array([[deathDate, useDate], [deathDate2, useDate2], [deathDate3, useDate3]], dtype=str)
+        headers = ["date_mort", "date_utilisation"]
+        dataframe = pd.DataFrame(array, columns=headers)
+        dataframe["delay"] = dataframe.apply(lambda row: DataframeUtils.nbDaysBetweenDeathAndUse(row), axis=1)
+        newArray = [[deathDate, useDate, np.nan], [deathDate2, useDate2, np.nan], [deathDate3, useDate3, np.nan]]
+        newHeaders = ["date_mort", "date_utilisation", "delay"]
+        finalDataframe = pd.DataFrame(newArray, columns=newHeaders)
+        self.assertTrue(finalDataframe.equals(dataframe))
+
+    def testInjectionVolumeFloatInString(self):
+        injectionVolume = "300"
+        injectionVolume2 = " 300"
+        injectionVolume3 = "   300.36"
+        injectionVolume4 = "0.25"
+        array = np.array([[injectionVolume], [injectionVolume2], [injectionVolume3], [injectionVolume4]])
+        headers = ["volume_injection"]
+        dataframe = pd.DataFrame(array, columns=headers)
+        dataframe["injectionVolume"] = dataframe.apply(lambda row: DataframeUtils.injectionVolume(row), axis=1)
+        newArray = [[injectionVolume, 300], [injectionVolume2, 300], [injectionVolume3, 300.36],
+                    [injectionVolume4, 0.25]]
+        headers = ["volume_injection", "injectionVolume"]
+        finalDataframe = pd.DataFrame(newArray, columns=headers)
+        self.assertTrue(finalDataframe.equals(dataframe))
+
+    def testInjectionVolume2x200(self):
+        injectionVolume = [["2 x 200 nl (G+D)"], ["2 x 200 nl (G+D)"]]
+        headers = ["volume_injection"]
+        dataframe = pd.DataFrame(injectionVolume, columns=headers)
+        dataframe["injectionVolume"] = dataframe.apply(lambda row: DataframeUtils.injectionVolume(row), axis=1)
+        newArray = [["2 x 200 nl (G+D)", 400], ["2 x 200 nl (G+D)", 400]]
+        newHeaders = ["volume_injection", "injectionVolume"]
+        finalDataframe = pd.DataFrame(newArray, columns=newHeaders)
+        self.assertTrue(finalDataframe.equals(dataframe))
+
+    def testInjectionVolumeNotValidValues(self):
+        injectionVolume = [["2 x 20000 nl (G+D)"], ["2 x 20000 nl (G+D)"]]
+        headers = ["volume_injection"]
+        dataframe = pd.DataFrame(injectionVolume, columns=headers)
+        dataframe["injectionVolume"] = dataframe.apply(lambda row: DataframeUtils.injectionVolume(row), axis=1)
+        newArray = [["2 x 20000 nl (G+D)", np.nan], ["2 x 20000 nl (G+D)", np.nan]]
+        newHeaders = ["volume_injection", "injectionVolume"]
+        finalDataframe = pd.DataFrame(newArray, columns=newHeaders)
+        self.assertTrue(finalDataframe.equals(dataframe))
+
+    def testUtilMethodsDropRows(self):
+        array = np.array([[0, 1, 2, 3], [1, 2, 3, 4], [2, 3, 4, -25]])
+        headers = ["A", "B", "C", "D"]
+        dataframe = pd.DataFrame(array, columns=headers)
+        newDataframe = DataframeUtils.dropRowsWithCertainValues(dataframe, -25, "D")
+        self.assertFalse(dataframe.equals(newDataframe))
+
+    def testUtilUsageStats(self):
+        array = np.array([[np.nan, 0, 1, 2], [1, np.nan, 1, 2]])
+        headers = ["a", "b", "c", "d"]
+        dataframe = pd.DataFrame(array, columns=headers)
+        stats = DataframeUtils.usedValuesStatsPerColumn(dataframe)
+        supposedDict = {"a": (1 / 2, 1, 2), "b": (1 / 2, 1, 2), "c": (1, 2, 2), "d": (1, 2, 2)}
+        self.assertEqual(supposedDict, stats)
 
 
 if __name__ == '__main__':
