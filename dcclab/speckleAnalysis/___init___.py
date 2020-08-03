af9ed796526bab7d255542a472873ec195bf4025
Gabriel Genest <gabriel.genest.2@ulaval.ca>
2020-08-03 18:42:41 -0400

    Create ___init___.py

---
+++ dcclab/speckleAnalysis/___init___.py
@@ -0,0 +1,7 @@
+""" We import almost everything by default, in the general
+namespace because it is simpler for everyone """
+
+from .autocorrelation import *
+from .peakMeasurement import *
+from .sourceSimulations import *
+from .speckleCaracterization import *
