db75e37727235d27c2f7b9510481b9502232074e
Gabriel Genest <gabriel.genest.2@ulaval.ca>
2020-08-05 17:17:24 -0400

    fixed report display (graphs + text)

--- dcclab/speckleAnalysis/speckleStatsReport.py
+++ dcclab/speckleAnalysis/speckleStatsReport.py
@@ -77,14 +77,14 @@ class SpeckleStatsReport:
         plt.show()
 
     def displayIntensityHistogram(self):
-        plt.hist(self.__image, self.__intensityBins)
+        plt.hist(self.__image.ravel(), self.__intensityBins)
         plt.show()
 
     def displayLocalContrastHistogram(self):
-        plt.hist(self.__localContrast, self.__localContrastBins)
+        plt.hist(self.__localContrast.ravel(), self.__localContrastBins)
         plt.show()
 
-    def __str__(self):
+    def textReport(self, split: bool = False):
         header = "========== Statistical properties of the speckle image ==========\n"
         basicStats = f"Mean intensity : {self.meanIntensity}\nIntensity std deviation : {self.stdDevIntensity}\n"
         basicStats += f"Maximum intensity : {self.maxIntensity}\nMinimum intensity : {self.minIntensity}\n"
@@ -92,25 +92,40 @@ class SpeckleStatsReport:
         midSection = "========== Statistical properties of the speckles ==========\n"
         speckleStats = f"Vertical diam. : {self.verticalFWHM} pixels\nHorizontal diam. : {self.horizontalFWHM} pixels\n"
         speckleStats += self.__speckleCaracterizationObj.FWHMFindingMethodInfo() + "\n"
-        fullyDeveloped = "This is not a fully developed speckle pattern (based on the intensity histogram, " \
-                         "its maximum is not at 0, not assuming exponential distribution)\n"
+        fullyDeveloped = "This is not a fully developed speckle pattern\n"
+        fullyDeveloped += "(based on the intensity histogram, its maximum is not at 0, not assuming exponential " \
+                          "distribution)\n"
         if self.isFullyDevelopped:
-            fullyDeveloped = "This is a fully developed speckle pattern (based on the intensity histogram, " \
-                             "its maximum is at 0, assuming exponential distribution)\n"
+            fullyDeveloped = "This is a fully developed speckle pattern\n"
+            fullyDeveloped += "(based on the intensity histogram, its maximum is at 0, assuming exponential " \
+                              "distribution)\n"
         speckleStats += fullyDeveloped
         localContrastStats = f"Local contrast mean : {np.mean(self.__localContrast)}\n"
         localContrastStats += f"Local contrast std deviation : {np.std(self.__localContrast)}\n"
         localContrastStats += f"Local contrast median : {np.median(self.__localContrast)}\n"
         localContrastStats += f"Local contrast min : {np.min(self.__localContrast)}\n"
         localContrastStats += f"Local contrast max : {np.max(self.__localContrast)}"
+        if split:
+            allText = (header + basicStats, midSection + speckleStats + localContrastStats)
+        else:
+            allText = header + basicStats + midSection + speckleStats + localContrastStats
+        return allText
 
+    def __str__(self):
+        return self.textReport()
 
-    def speckleStatsReport(self):
-        fig, axes = plt.subplots(2, 2)
+    def speckleStatsReport(self, saveName: str = None):
+        fig, axes = plt.subplots(3, 2)
+        fig.subplots_adjust(hspace=0.4)
         ax1 = axes[0, 0]  # Top left
         ax2 = axes[0, 1]  # Top right
         ax3 = axes[1, 0]  # Bottom left
         ax4 = axes[1, 1]  # Bottom right
+        ##### Text info #####
+        gs = axes[2, 0].get_gridspec()
+        for ax in axes[2, 0:]:
+            ax.remove()
+        axBig = fig.add_subplot(gs[2, 0:])
         fig.suptitle(f"Speckles statistical report of\n{self.__imagePath}")
 
         ax1.imshow(self.__image)
@@ -119,17 +134,35 @@ class SpeckleStatsReport:
         ax2.imshow(self.__localContrast)
         ax2.set_title(f"Local contrast (kernel size of {self.__localContrastKernelSize})")
 
-        ax3.hist(self.__image, self.__intensityBins)
-        title = f"Intensity histogram ({len(self.__intensityBins) - 1} bins, ranging from {self.__intensityBins[0]} to "
+        ax3.hist(self.__image.ravel(), self.__intensityBins)
+        title = f"Intensity histogram\n({len(self.__intensityBins) - 1} bins, ranging from {self.__intensityBins[0]} to "
         title += f"{self.__intensityBins[-1]})"
         ax3.set_title(title)
         ax3.set_xlabel("Intensity value [-]")
         ax3.set_ylabel("Count [-]")
 
-        ax4.hist(self.__localContrast, self.__localContrastBins)
-        title = f"Local contrast histogram (kernel size of {self.__localContrastKernelSize}, "
-        title += f"{len(self.__intensityBins) - 1} bins, ranging from {self.__intensityBins[0]} to "
-        title += f"{self.__intensityBins[-1]})"
+        ax4.hist(self.__localContrast.ravel(), self.__localContrastBins)
+        title = f"Local contrast histogram\n({len(self.__localContrastBins) - 1} bins, ranging"
+        title += f" from {np.round(self.__localContrastBins[0], 2)} to {np.round(self.__localContrastBins[-1], 2)})"
         ax4.set_title(title)
         ax4.set_xlabel("Local contrast value [-]")
         ax4.set_ylabel("Count [-]")
+
+        text = self.textReport(False)
+        axBig.text(0.5, -0.1, text, ha="center", fontsize=8)
+        axBig.axis("off")
+        mng = plt.get_current_fig_manager()
+        mng.resize(*mng.window.maxsize())  # Works only with Tk backend I think... This is only for aesthetic
+        fig.set_size_inches((8.5, 11), forward=False)
+        if saveName is not None:
+            plt.savefig(fname=saveName, dpi=1000)
+        plt.show()
+
+    def fullMethodInfo(self):
+        raise NotImplementedError("To do")
+
+
+if __name__ == '__main__':
+    path = r"..\speckleAnalysis\circularWithPhasesSimulations\4pixelsCircularWithPhasesSimulations.tiff"
+    ssr = SpeckleStatsReport(path, averageRange=20 / 100)
+    ssr.speckleStatsReport("test.pdf")
